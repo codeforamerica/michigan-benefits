b3f4dbeb4e898638881b51e5cc7c2887ec778f7b
Tom Lieber <tom@alltom.com>
2014-10-01 17:49:30 -0700

    improve coverage of signup/sessions to 100%

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,14 +1,29 @@
 require 'test_helper'
 
-class SessionsControllerTest < ActionController::TestCase
-  test "should get login" do
+describe SessionsController do
+  it "should get login" do
     get :new
     assert_response :success
   end
 
-  test "should get logout" do
+  it "should get logout" do
     get :destroy
     assert_response :redirect
   end
 
+  describe '#create' do
+    it 'success' do
+      Account.create!(email: 'bob@example.com', password: 'password')
+
+      post :create, {email: 'bob@example.com', password: 'password'}
+      assert_response :redirect
+      assert_redirected_to my_account_path
+    end
+
+    it 'failure' do
+      post :create, {email: 'bob@example.com', password: 'password'}
+      assert_response :success
+      assert_template 'sessions/new'
+    end
+  end
 end
