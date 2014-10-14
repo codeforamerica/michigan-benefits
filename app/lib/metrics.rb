1af6fd98a0199cb4c77fce3eab9c93010f17dde5
Jacob Maine <pair+jacob@pivotallabs.com>
2014-10-14 10:33:09 -0700

    Add metrics for signups per day

--- app/lib/metrics.rb
+++ app/lib/metrics.rb
@@ -9,4 +9,14 @@ class Metrics
       { start: start_date, finish: end_date, count: count }
     end
   end
+
+  def self.signups_per_day
+    7.times.map do |i|
+      start_date = 6.days.ago.to_date + i.days
+      start_time = start_date.beginning_of_day
+      end_time = start_date.end_of_day
+      count = Account.where(created_at: (start_time..end_time)).count
+      { start: start_date, count: count }
+    end
+  end
 end
--- spec/app/lib/metrics_spec.rb
+++ spec/app/lib/metrics_spec.rb
@@ -16,7 +16,7 @@ describe Metrics do
       create(:account)
       create(:account)
     end
-    
+
     travel_to Time.zone.local(2014, 10, 31) do # Friday
       metrics = Metrics.signups_per_week
 
@@ -32,5 +32,32 @@ describe Metrics do
     end
   end
 
-  it "returns signups per day in the last week"
+  it "returns signups per day in the last week" do
+    travel_to Time.zone.local(2014, 10, 24) do
+      create(:account)
+    end
+
+    travel_to Time.zone.local(2014, 10, 25) do
+      create(:account)
+      create(:account)
+      create(:account)
+    end
+
+    travel_to Time.zone.local(2014, 10, 27) do
+      create(:account)
+      create(:account)
+    end
+
+    travel_to Time.zone.local(2014, 10, 31) do # Friday
+      metrics = Metrics.signups_per_day
+
+      expect(metrics.length).to eq(7)
+      expect(metrics[0][:count]).to eq(3)
+      expect(metrics[1][:count]).to eq(0)
+      expect(metrics[2][:count]).to eq(2)
+
+      expect(metrics[0][:start]).to eq(Date.new(2014, 10, 25))
+      expect(metrics[1][:start]).to eq(Date.new(2014, 10, 26))
+    end
+  end
 end
