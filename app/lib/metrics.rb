76bde25512d0ee2aa39204904e885d8796b4587a
Jacob Maine & Tom Lieber <pair+jacob+tom@citizencode.io>
2014-10-14 14:45:41 -0700

    Calculate churn metrics

--- app/lib/metrics.rb
+++ app/lib/metrics.rb
@@ -1,22 +1,44 @@
 class Metrics
-  def self.signups_per_week
-    12.times.map do |i|
-      start_date = 11.weeks.ago.beginning_of_week.to_date + i.weeks
-      end_date = start_date + 6.days
-      start_time = start_date.beginning_of_day
-      end_time = end_date.end_of_day
-      count = Account.where(created_at: (start_time..end_time)).count
-      { start: start_date, finish: end_date, count: count }
+  class << self
+    def signups_per_week
+      count_per_week(Account, :created_at)
     end
-  end
 
-  def self.signups_per_day
-    7.times.map do |i|
-      start_date = 6.days.ago.to_date + i.days
-      start_time = start_date.beginning_of_day
-      end_time = start_date.end_of_day
-      count = Account.where(created_at: (start_time..end_time)).count
-      { start: start_date, count: count }
+    def signups_per_day
+      count_per_day(Account, :created_at)
+    end
+
+    def churn_per_week
+      lapsed = Account.where("last_activity_at < ?", 11.weeks.ago.beginning_of_week.beginning_of_day).count
+      [{ start: -Date::Infinity.new, count: lapsed}] +
+        count_per_week(Account, :last_activity_at)
+    end
+
+    def churn_per_day
+      count_per_day(Account, :last_activity_at)
+    end
+
+    private
+
+    def count_per_week(scope, attr)
+      12.times.map do |i|
+        start_date = i.weeks.ago.beginning_of_week.to_date
+        end_date = start_date + 6.days
+        start_time = start_date.beginning_of_day
+        end_time = end_date.end_of_day
+        count = scope.where(attr => (start_time..end_time)).count
+        { start: start_date, count: count }
+      end.reverse
+    end
+
+    def count_per_day(scope, attr)
+      7.times.map do |i|
+        start_date = i.days.ago.to_date
+        start_time = start_date.beginning_of_day
+        end_time = start_date.end_of_day
+        count = scope.where(attr => (start_time..end_time)).count
+        { start: start_date, count: count }
+      end.reverse
     end
   end
 end
--- spec/app/lib/metrics_spec.rb
+++ spec/app/lib/metrics_spec.rb
@@ -3,61 +3,113 @@ require 'rails_helper'
 describe Metrics do
   it "returns signups per week (last 12 weeks)" do
     travel_to Time.zone.local(2014, 4, 1) do
-      create(:account)
+      1.times { create(:account) }
     end
 
     travel_to Time.zone.local(2014, 8, 11) do
-      create(:account)
-      create(:account)
-      create(:account)
+      3.times { create(:account) }
     end
 
     travel_to Time.zone.local(2014, 8, 26) do
-      create(:account)
-      create(:account)
+      2.times { create(:account) }
     end
 
     travel_to Time.zone.local(2014, 10, 31) do # Friday
       metrics = Metrics.signups_per_week
 
-      expect(metrics.length).to eq(12)
       expect(metrics[0][:count]).to eq(3)
       expect(metrics[1][:count]).to eq(0)
       expect(metrics[2][:count]).to eq(2)
 
       expect(metrics[0][:start]).to eq(Date.new(2014, 8, 11))
-      expect(metrics[0][:finish]).to eq(Date.new(2014, 8, 17))
       expect(metrics[1][:start]).to eq(Date.new(2014, 8, 18))
-      expect(metrics[1][:finish]).to eq(Date.new(2014, 8, 24))
+      expect(metrics.length).to eq(12)
     end
   end
 
   it "returns signups per day in the last week" do
     travel_to Time.zone.local(2014, 10, 24) do
-      create(:account)
+      1.times { create(:account) }
     end
 
     travel_to Time.zone.local(2014, 10, 25) do
-      create(:account)
-      create(:account)
-      create(:account)
+      3.times { create(:account) }
     end
 
     travel_to Time.zone.local(2014, 10, 27) do
-      create(:account)
-      create(:account)
+      2.times { create(:account) }
     end
 
     travel_to Time.zone.local(2014, 10, 31) do # Friday
       metrics = Metrics.signups_per_day
 
-      expect(metrics.length).to eq(7)
       expect(metrics[0][:count]).to eq(3)
       expect(metrics[1][:count]).to eq(0)
       expect(metrics[2][:count]).to eq(2)
 
       expect(metrics[0][:start]).to eq(Date.new(2014, 10, 25))
       expect(metrics[1][:start]).to eq(Date.new(2014, 10, 26))
+      expect(metrics.length).to eq(7)
+    end
+  end
+
+  it "returns count of users who were last active x weeks ago" do
+    mark_active = Proc.new { |a| a.update_single_attribute(:last_activity_at, Time.current) }
+    accounts = nil
+    travel_to Time.zone.local(2014, 4, 1) do
+      accounts = 6.times.map { create(:account) }
+      mark_active.call(accounts[0])
+    end
+
+    travel_to Time.zone.local(2014, 8, 11) do
+      accounts[1..3].each(&mark_active)
+    end
+
+    travel_to Time.zone.local(2014, 8, 26) do
+      accounts[4..5].each(&mark_active)
+    end
+
+    travel_to Time.zone.local(2014, 10, 31) do # Friday
+      metrics = Metrics.churn_per_week
+
+      expect(metrics[0][:count]).to eq(1)
+      expect(metrics[1][:count]).to eq(3)
+      expect(metrics[2][:count]).to eq(0)
+      expect(metrics[3][:count]).to eq(2)
+
+      expect(metrics[0][:start]).to eq(-Date::Infinity.new)
+      expect(metrics[1][:start]).to eq(Date.new(2014, 8, 11))
+      expect(metrics[2][:start]).to eq(Date.new(2014, 8, 18))
+      expect(metrics.length).to eq(13)
+    end
+  end
+
+  it "returns count of users who were last active x days ago" do
+    mark_active = Proc.new { |a| a.update_single_attribute(:last_activity_at, Time.current) }
+    accounts = nil
+    travel_to Time.zone.local(2014, 10, 24) do
+      accounts = 6.times.map { create(:account) }
+      mark_active.call(accounts[0])
+    end
+
+    travel_to Time.zone.local(2014, 10, 25) do
+      accounts[1..3].each(&mark_active)
+    end
+
+    travel_to Time.zone.local(2014, 10, 27) do
+      accounts[4..5].each(&mark_active)
+    end
+
+    travel_to Time.zone.local(2014, 10, 31) do # Friday
+      metrics = Metrics.churn_per_day
+
+      expect(metrics[0][:count]).to eq(3)
+      expect(metrics[1][:count]).to eq(0)
+      expect(metrics[2][:count]).to eq(2)
+
+      expect(metrics[0][:start]).to eq(Date.new(2014, 10, 25))
+      expect(metrics[1][:start]).to eq(Date.new(2014, 10, 26))
+      expect(metrics.length).to eq(7)
     end
   end
 end
