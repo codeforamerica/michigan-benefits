2f3d219ed7d9f5f975266a63447a31a4889b0dfb
Jacob Maine <pair+jmaine@citizencode.io>
2014-10-29 17:18:37 -0700

    Make sidebar collapse on small screens

--- app/assets/javascripts/sidebar-height.js
+++
@@ -1,9 +0,0 @@
-function setContainerHeight() {
-  var tabbarHeight = 45; // from $tabbar-height in foundation_and_overrides.scss
-  var height = $(document).height() - tabbarHeight;
-  $('.app-nav').height(height);
-  $('.app-aside').height(height);
-}
-
-$(document).ready(setContainerHeight);
-$(window).resize(setContainerHeight);
