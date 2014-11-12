6a90c88c128738c53595d2ce6f93df05de5c1342
Jacob Maine <pair+jmaine@citizencode.io>
2014-11-11 16:43:19 -0800

    Add email button; document email template structure

--- app/views/email_base.rb
+++ app/views/email_base.rb
@@ -1,4 +1,33 @@
 module Views
+  # Quick Tutorial on Structuring Emails
+  #
+  # We're using Zurb Ink to make structured, responsive emails. Ink doesn't get
+  # away from tables (that's still not possible) but it helps you style them.
+  #
+  # This file has a number of helpers for generating the nested tables that are
+  # required.  See the [Zurb docs](http://zurb.com/ink/docs.php) for
+  # background, details and more possibilities.
+  #
+  # For the most part, you can use the `row` and `column` helpers that should
+  # be familiar from Views::Base.  They work slightly differently and have some
+  # caveats, explained here.
+  #
+  # 1. Columns do not have small/medium/large variants; their sizes are
+  #    spelled-out numbers: *three*, *four*, *six*, *twelve*, etc.  All columns
+  #    collapse at 580px.
+  # 2. The last column in a row must use the `last_column` helper.
+  # 3. Rows are not nestable.  You may want to look at sub-grids (non-
+  #    responsive) or block-grids (which collapse according to usual div rules)
+  #    if you need something similar to nested rows.
+  # 4. The block passed to `column` or `full_row` must return a `td`, or a
+  #    series of `td`s if you are using sub-grids.
+  #
+  # There are also helpers for centering content, creating buttons and creating
+  # full-width rows.
+  #
+  # There are no helpers, yet, for sub-grids, block-grids, panels, retina-
+  # images, or hidden content for small screens, though they may all be
+  # possible.
   class EmailBase < Fortitude::Widget
     doctype :xhtml10_transitional
 
@@ -71,8 +100,9 @@ module Views
       td(add_classes(args, [:center]).merge(align: :center)) { center(&block) }
     end
 
-    # def buttonish(size = :small, *extras)
-    # end
+    def buttonish(size = :small, args = {}, &block)
+      table(add_classes(args, [:"#{size}-button"])) { tr { td(&block) } }
+    end
 
     def add_classes(args, classes)
       classes += Array(args.fetch(:class, []))
