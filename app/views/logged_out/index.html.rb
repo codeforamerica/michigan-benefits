a15c77f768fb2c77867235a91d21d62e37ab0cec
Heather Moore & Jacob Maine <pair+heather+jmaine@citizencode.io>
2015-03-10 12:39:20 -0700

    Remove signup and login links, until clients have a need for them

--- app/views/logged_out/index.html.rb
+++ app/views/logged_out/index.html.rb
@@ -3,16 +3,6 @@ class Views::LoggedOut::Index < Views::Base
     full_row {
       h1("LoggedOut#index")
       p("Find me in app/views/logged_out/index.html.erb")
-      p("Testing button styles and colors below...")
-      row {
-        column(%i[small-12 large-6]) {
-          link_to 'Sign Up', new_account_path, class: buttonish(:medium, :primary, :expand)
-        }
-
-        column(%i[small-12 large-6]) {
-          link_to 'Log in', new_session_path, class: buttonish(:medium, :success, :expand)
-        }
-      }
     }
   end
 end
