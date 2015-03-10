a79739cf144e7eeb5272501db25d664868e18121
Heather Moore & Jacob Maine <pair+heather+jmaine@citizencode.io>
2015-03-10 14:37:09 -0700

    add primary marketing section on homepage

--- app/views/logged_out/index.html.rb
+++ app/views/logged_out/index.html.rb
@@ -1,8 +1,27 @@
 class Views::LoggedOut::Index < Views::Base
   def content
-    full_row {
-      h1("LoggedOut#index")
-      p("Find me in app/views/logged_out/index.html.erb")
+    row(class: %i[fullWidth background-cover]) {
+      column {
+        row {
+          column(:'medium-8', class: :'medium-offset-2') {
+            br
+            div(class: %i[clear-panel text-center]) {
+              h1 {
+                span "Slogan", class: :'text-primary'
+                br
+                span "Tagline", class: :'text-alert'
+              }
+              h4 "Solution X for Problem Y"
+
+              p "Why this is right for you. " * 8
+
+              div(class: buttonish(:large, :alert)) {
+                text "Take Action"
+              }
+            }
+          }
+        }
+      }
     }
   end
 end
