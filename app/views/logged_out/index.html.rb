3a16617fe1a5d8aa340cea17ace63ceb2f57de16
Heather Moore & Jacob Maine <pair+heather+jmaine@citizencode.io>
2015-03-10 15:33:05 -0700

    Add a user testimonial

--- app/views/logged_out/index.html.rb
+++ app/views/logged_out/index.html.rb
@@ -1,24 +1,40 @@
 class Views::LoggedOut::Index < Views::Base
   def content
-    row(class: %i[fullWidth background-cover]) {
+    wide_row(:'background-cover') {
+      div(class: %i[clear-panel text-center]) {
+        h1 {
+          span "Slogan", class: :'text-primary'
+          br
+          span "Tagline", class: :'text-alert'
+        }
+        h4 "Solution X for Problem Y"
+
+        p "Why this is right for you. " * 8
+
+        div(class: buttonish(:large, :alert)) {
+          text "Take Action"
+        }
+      }
+    }
+    wide_row(:'palate-white-on-green') {
+      blockquote(class: :'blockquote-grande') {
+        text "“It's really good”"
+        cite "First User"
+      }
+    }
+  end
+
+  private
+
+  def wide_row(add_class)
+    classes = [:fullWidth]
+    classes << add_class
+    row(class: classes) {
       column {
         row {
           column(:'medium-8', class: :'medium-offset-2') {
             br
-            div(class: %i[clear-panel text-center]) {
-              h1 {
-                span "Slogan", class: :'text-primary'
-                br
-                span "Tagline", class: :'text-alert'
-              }
-              h4 "Solution X for Problem Y"
-
-              p "Why this is right for you. " * 8
-
-              div(class: buttonish(:large, :alert)) {
-                text "Take Action"
-              }
-            }
+            yield
           }
         }
       }
