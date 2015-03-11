c7b13da9c663f16962f2466e79f36cb67cac9205
Heather Moore <pair+heather@citizencode.io>
2015-03-11 10:15:37 -0700

    add call to action to right nav bar

--- app/views/layouts/raw.rb
+++ app/views/layouts/raw.rb
@@ -10,48 +10,61 @@ class Views::Layouts::Raw < Views::Base
 
         title(content_for?(:title) ? yield(:title) : "CitizenRails")
 
-        stylesheet_link_tag    'application', media: 'all', 'data-turbolinks-track' => true
-        stylesheet_link_tag    '//fonts.googleapis.com/css?family=Lato|Slabo+27px'
+        stylesheet_link_tag 'application', media: 'all', 'data-turbolinks-track' => true
+        stylesheet_link_tag '//fonts.googleapis.com/css?family=Lato|Slabo+27px'
         javascript_include_tag 'vendor/modernizr'
         javascript_include_tag 'application', 'data-turbolinks-track' => true
         csrf_meta_tags
       }
 
       body {
-        nav(:class => "top-bar", "data-topbar" => "", :role => "navigation") {
-          ul(:class => "title-area") {
-            li(:class => "name") {
-              h1 {
-                a(:href => "/") {
-                  image_tag "http://placekitten.com/g/90/57", class: "logo", alt: "Citizen Code Logo"
+        div(class: "contain-to-grid") {
+          nav(:class => "top-bar", "data-topbar" => "", :role => "navigation") {
+            ul(:class => "title-area") {
+              li(:class => "name") {
+                h1 {
+                  a(:href => "/") {
+                    image_tag "http://placekitten.com/g/90/57", class: "logo", alt: "Citizen Code Logo"
+                  }
+                }
+              }
+
+              li(:class => "toggle-topbar menu-icon") {
+                a(:href => "#") {
+                  span("Menu")
                 }
               }
             }
 
-            li(:class => "toggle-topbar menu-icon") {
-              a(:href => "#") {
-                span("Menu")
+            section(class: "top-bar-section") {
+              ul(class: "right") {
+                li(class: "active") {
+                  a(href: "#") {
+                    text "Call to action"
+                  }
+                }
               }
             }
+
+            yield :navigation
           }
 
-          yield :navigation
+          flash.each do |name, msg|
+            div("aria-labelledby" => "flash-msg-#{name}", "aria-role" => "dialog", :class => "alert-box flash-msg #{name}", "data-alert" => "") {
+              div(msg, :id => "flash-msg-#{name}")
+              a("×", :class => "close", :href => "#")
+            }
+          end
+
+
+          content_for?(:body) ? yield(:body) : yield
+
+          if content_for?(:footer)
+            footer(class: :'fat-footer') {
+              yield :footer
+            }
+          end
         }
-
-        flash.each do |name, msg|
-          div("aria-labelledby" => "flash-msg-#{name}", "aria-role" => "dialog", :class => "alert-box flash-msg #{name}", "data-alert" => "") {
-            div(msg, :id => "flash-msg-#{name}")
-            a("×", :class => "close", :href => "#")
-          }
-        end
-
-        content_for?(:body) ? yield(:body) : yield
-
-        if content_for?(:footer)
-          footer(class: :'fat-footer') {
-            yield :footer
-          }
-        end
       }
     }
   end
