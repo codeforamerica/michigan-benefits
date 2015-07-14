684f562ff16685ba2bc9114ec596853e73110420
David Balatero <dbalatero@gmail.com>
2015-07-14 14:31:05 -0700

    Add message to initializer to enforce encrypted cookies on production

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: "_#{Rails.application.config.project_slug}_session"
+if Rails.env.production? && !ENV.key?('SECRET_KEY_BASE')
+  raise "Please run `rake secret` and configure ENV['SECRET_KEY_BASE'] "\
+    "on production, or else session cookies will not be encrypted."
+end
+
+Rails.application.config.session_store :cookie_store,
+  key: "_#{Rails.application.config.project_slug}_session"
