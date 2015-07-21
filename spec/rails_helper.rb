e5c7a8265e0a52c7c4fba49ce37fe2f8326ca890
David Balatero <dbalatero@gmail.com>
2015-07-21 16:07:48 -0700

    Quiet core gems in RSpec stacktraces

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -56,4 +56,13 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  config.filter_gems_from_backtrace \
+    "actionpack",
+    "actionview",
+    "activesupport",
+    "airbrake",
+    "rack",
+    "railties",
+    "zeus"
 end
