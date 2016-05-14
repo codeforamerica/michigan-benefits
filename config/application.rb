f2cf72ff66f5e4213824bd781960e34843e7b639
Erik Hanson <pair+erik@citizencode.io>
2016-05-13 21:23:23 -0700

    rename module

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module CitizenRailsReboot
+module CitizenRails
   class Application < Rails::Application
     config.site_name = "Citizen Rails"
     config.project_description = ""
