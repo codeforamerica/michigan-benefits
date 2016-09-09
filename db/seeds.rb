93fe0391a6f6ae5dc49ea94805fd43e42f27c920
Erik Hanson and Glenn Jahnke <erik+glenn@gmail.com>
2016-09-09 10:49:27 -0700

    make admin and member users when seeding db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+def create_user(email, admin)
+  name = email.split("@").first.capitalize
+  user = User.find_or_initialize_by(email: email)
+  if user.new_record?
+    puts "Creating user #{name} <#{email}>"
+    user.update!(name: name, admin: admin, password: "password")
+  end
+end
+
+unless Rails.env.production?
+  create_user("admin@example.com", true)
+  create_user("member@example.com", false)
+end
