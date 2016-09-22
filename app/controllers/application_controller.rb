721869720edfba7e855f4d85785d23afb862a59c
Erik Hanson <pair+erik@citizencode.io>
2016-09-22 07:04:29 -0700

    Feature flag for forcing SSL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception # Prevent CSRF attacks by raising an exception. For APIs, you may want to use :null_session instead.
+  force_ssl if: -> { Feature.ssl? }
   before_action :basic_auth, :verify_allowed_user
 
   respond_to :html
---
+++ app/lib/feature.rb
@@ -0,0 +1,11 @@
+class Feature
+  def self.ssl?
+    enabled? "SSL"
+  end
+
+  private
+
+  def self.enabled?(feature)
+    ENV["#{feature}_ENABLED"] == "true"
+  end
+end
