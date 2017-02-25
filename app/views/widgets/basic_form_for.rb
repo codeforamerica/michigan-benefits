68f9f2edab3204f0da3c7d021cfe25242483bd31
bion <bionjohnson@gmail.com>
2017-02-24 16:43:18 -0800

    Add required option to BasicFormFor

--- app/views/widgets/basic_form_for.rb
+++ app/views/widgets/basic_form_for.rb
@@ -2,30 +2,39 @@ class Views::Widgets::BasicFormFor < Views::Base
   needs :form_for_params, :title, :definition
 
   def content
-    definition.call(self)
-
-    row "align-center" do
+    row do
       column medium: 6 do
         h2 title
 
-        form_for *form_for_params do |f|
+        form_for(*form_for_params) do |f|
+          definition.call(self)
+
           fields.each.with_index do |form_field, index|
             errors = f.object.present? ? f.object.errors[form_field.name] : nil
+
             f.label(form_field.name) do
               text form_field.title
-              span errors.to_sentence, class: "error-message" if errors.present?
-              text f.send("#{form_field.type}_field".to_sym, form_field.name, autofocus: index == 0)
+              span(' &mdash; required'.html_safe, class: 'required') if form_field.required
+              span errors.to_sentence, class: 'error-message' if errors.present?
+
+              if form_field.type.to_sym == :text_area
+                text f.text_area(form_field.name, autofocus: index == 0)
+              elsif form_field.type.to_sym == :attachment
+                text f.attachment_field(form_field.name)
+              else
+                text f.send("#{form_field.type}_field".to_sym, form_field.name, autofocus: index == 0)
+              end
             end
           end
 
-          f.submit "Continue", class: "button"
+          f.submit 'Continue', class: 'button special'
         end
       end
     end
   end
 
-  def field(name, type: :text, title: name.to_s.humanize)
-    fields << OpenStruct.new(type: type, name: name, title: title)
+  def field(name, type: :text, title: name.to_s.humanize, required: false)
+    fields << OpenStruct.new(type: type, name: name, title: title, required: required)
   end
 
   private
--- spec/views/widgets/basic_form_for_spec.rb
+++ spec/views/widgets/basic_form_for_spec.rb
@@ -5,7 +5,7 @@ describe "widgets/basic_form_for.html.rb" do
     user = User.new
 
     definition = -> (f) do
-      f.field :email, title: "Email Address", type: :email
+      f.field :email, title: "Email Address", type: :email, required: true
       f.field :name, title: "Full Name"
       f.field :name
 
@@ -18,20 +18,20 @@ describe "widgets/basic_form_for.html.rb" do
 
     render
 
-    expect(rendered).to eq <<-HTML.squish.gsub(/>(\s)+</, '><')
-      <div class="align-center expanded row">
+    expect(rendered).to match_html <<-HTML
+      <div class="expanded row">
         <div class="columns small-12 medium-6">
           <h2>User Details</h2>
 
           <form class="new_user" id="new_user" action="/user/new" accept-charset="UTF-8" method="post">
             <input name=\"utf8\" type=\"hidden\" value=\"&#x2713;\" />
 
-            <label for="user_email">Email Address<input autofocus="autofocus" type="email" name="user[email]" id="user_email" /></label>
+            <label for="user_email">Email Address<span class="required"> required</span><input autofocus="autofocus" type="email" name="user[email]" id="user_email" /></label>
             <label for="user_name">Full Name<input type="text" name="user[name]" id="user_name" /></label>
             <label for="user_name">Name<input type="text" name="user[name]" id="user_name" /></label>
             <label for="user_password">Password<input type="password" name="user[password]" id="user_password" /></label>
 
-            <input type="submit" name="commit" value="Continue" class="button" data-disable-with="Continue" />
+            <input type="submit" name="commit" value="Continue" class="button special" data-disable-with="Continue" />
           </form>
         </div>
       </div>
