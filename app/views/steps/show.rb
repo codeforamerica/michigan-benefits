b34d0077624f0ff73eb4b453958c354a59153a06
David Balatero <david@citizencode.io>
2017-03-21 13:52:54 -0700

    Add menu header, break template into private methods

--- app/views/steps/show.rb
+++ app/views/steps/show.rb
@@ -2,13 +2,31 @@ class Views::Steps::Show < Views::Base
   needs :step
 
   def content
-    h1 step.title, class: "title"
+    content_for :template_name, "step"
+
+    menu_header
+    section_header
+    step_form
+  end
+
+  private
+
+  def menu_header
+    div class: 'step-header' do
+      div class: 'step-header__burger'
+
+      h4 step.title, class: 'step-header__title'
+    end
+  end
+
+  def section_header
     h2 step.headline, class: "headline"
     h3 step.subhead, class: "subhead"
+  end
 
-    form_for step, url: step_path(step), method: :put do |f|
+  def step_form
+    form_for step, as: :step, url: step_path(step), method: :put do |f|
       step.questions.each do |question, label_text|
-        field_name = "step[#{question}]"
         field_type = step.type(question)
 
         label label_text, "data-field-type" => field_type do
@@ -17,26 +35,28 @@ class Views::Steps::Show < Views::Base
           end
 
           case field_type
-            when :text
-              f.text_field question, name: field_name, placeholder: step.placeholder(question)
-            when :yes_no
-              div do
-                label do
-                  f.radio_button question, "true", name: field_name
-                  text "Yes"
-                end
-                label do
-                  f.radio_button question, "false", name: field_name
-                  text "No"
-                end
+          when :text
+            f.text_field question, placeholder: step.placeholder(question)
+          when :yes_no
+            div do
+              label do
+                f.radio_button question, "true"
+                text "Yes"
               end
-            else
-              raise "Unknown field type #{field_type}"
+
+              label do
+                f.radio_button question, "false"
+                text "No"
+              end
+            end
+          else
+            raise "Unknown field type #{field_type}"
           end
         end
       end
 
       link_to "Go back", step_path(step.previous) if step.previous.present?
+
       f.submit "Continue"
     end
   end
