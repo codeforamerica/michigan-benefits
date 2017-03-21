8dc511ee0bab78ca69ae11d214c10592affb2bb8
Erik Hanson <pair+erik@citizencode.io>
2017-03-21 15:54:05 -0700

    fix layout of homepage on wide screens, and fix floating header

--- app/views/users/new.html.rb
+++ app/views/users/new.html.rb
@@ -55,48 +55,50 @@ class Views::Users::New < Views::Base
 
   def apply_form
     div id: 'apply-for-programs', class: "slab slab--white" do
-      h2 "Choose the programs you want to apply for today."
-      p "You can choose more than one", class: "text--help"
+      div class: "card--narrow" do
+        h2 "Choose the programs you want to apply for today."
+        p "You can choose more than one", class: "text--help"
 
-      form_for user do |f|
-        program_selector \
+        form_for user do |f|
+          program_selector \
           icon: "health",
-          checked: true,
-          title: "Healthcare Coverage",
-          subtitle: "Medicaid, CHIP, and marketplace health insurance",
-          description: "Free or low-cost health coverage that helps pay for "\
+            checked: true,
+            title: "Healthcare Coverage",
+            subtitle: "Medicaid, CHIP, and marketplace health insurance",
+            description: "Free or low-cost health coverage that helps pay for "\
             "medical bills, doctors visits and prescriptions."
 
-        program_selector \
+          program_selector \
           icon: "food",
-          checked: true,
-          title: "Food Assistance",
-          subtitle: "Food Stamps, FAP or SNAP",
-          description: "Provides benefits to buy or grow food."
+            checked: true,
+            title: "Food Assistance",
+            subtitle: "Food Stamps, FAP or SNAP",
+            description: "Provides benefits to buy or grow food."
 
-        program_selector \
+          program_selector \
           icon: "cash",
-          title: "Cash Assistance",
-          subtitle: "FIP, RCA, and SDA",
-          description: "Temporary cash assistance for eligible pregnant "\
+            title: "Cash Assistance",
+            subtitle: "FIP, RCA, and SDA",
+            description: "Temporary cash assistance for eligible pregnant "\
             "women, low-income families with young children, refugees, "\
             "and adults with disabilities."
 
-        program_selector \
+          program_selector \
           icon: "childcare",
-          title: "Child Development + Care",
-          subtitle: "CDC",
-          description: "Helps pay for childcare."
+            title: "Child Development + Care",
+            subtitle: "CDC",
+            description: "Helps pay for childcare."
 
-        program_selector \
+          program_selector \
           icon: "ser",
-          title: "State Emergency Relief",
-          subtitle: "SER",
-          description: "Provides help or assistance for emergency situations."
+            title: "State Emergency Relief",
+            subtitle: "SER",
+            description: "Provides help or assistance for emergency situations."
 
-        button type: 'submit', class: "button button--cta button--full-width" do
-          text "Apply now"
-          i class: "button__icon icon-arrow_forward"
+          button type: 'submit', class: "button button--cta button--full-width" do
+            text "Apply now"
+            i class: "button__icon icon-arrow_forward"
+          end
         end
       end
     end
