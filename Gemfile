e3377888be33bb6cac1201f83f08c86ce2d416c3
David Balatero and Erik Hanson <david+erik@citizencode.io>
2017-03-24 14:28:32 -0700

    Add bucketeer config + paperclip config

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source "https://rubygems.org"
 ruby "2.3.1"
 
 gem "awesome_print"
+gem "aws-sdk"
 gem "bourbon", "~> 4.2.0" # to keep in sync with getcalfresh
 gem "coffee-rails", "~> 4.1.0"
 gem "fortitude"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,4 +98,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # AWS setup
+  credentials = AWS::Credentials.new(
+    ENV['BUCKETEER_AWS_ACCESS_KEY_ID'],
+    ENV['BUCKETEER_AWS_SECRET_ACCESS_KEY']
+  )
+
+  Aws.config.update(credentials: credentials)
+
+  config.paperclip_defaults = {
+    storage: :s3,
+    bucket: ENV['BUCKETEER_BUCKET_NAME']
+  }
 end
