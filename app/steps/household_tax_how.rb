95a18192bd01dc14bb5f799d6f4bd3b18ae4178c
Bion Johnson and Erik Hanson <bion+erik@citizencode.io>
2017-03-31 17:02:34 -0700

    hide label

--- app/steps/household_tax_how.rb
+++ app/steps/household_tax_how.rb
@@ -3,7 +3,9 @@ class HouseholdTaxHow < ManyMemberUpdateStep
   self.subhead = "Describe how your household files taxes."
   self.subhead_help = "If you aren't sure how your household files taxes right now, it is okay to skip these questions. We'll help you answer later."
 
-  self.member_questions = [:filing_status]
+  self.member_questions = {
+    filing_status: ["filing status", :hidden]
+  }
 
   self.types = {
     filing_status: :radios
