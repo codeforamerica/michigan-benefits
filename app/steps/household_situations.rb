2fe59a4d9428de5a87a78a58aba722cf0363bf0d
Erik Hanson <erik@citizencode.io>
2017-04-12 14:02:20 -0700

    skip household situations if none apply

--- app/steps/household_situations.rb
+++ app/steps/household_situations.rb
@@ -1,4 +1,13 @@
 class HouseholdSituations < ManyMemberUpdateStep
   self.title = "Your Household"
   self.subhead = "Ok, let us know which people these situations apply to."
+
+  def skip?
+    @app.everyone_a_citizen &&
+      ! @app.anyone_disabled &&
+      ! @app.any_new_moms &&
+      ! @app.any_medical_bill_help &&
+      ! @app.anyone_in_college &&
+      ! @app.anyone_living_elsewhere
+  end
 end
