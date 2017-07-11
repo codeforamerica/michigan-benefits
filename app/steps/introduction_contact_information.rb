a9bbf86667aac83247fcf8f8d32f756e1b59c553
Rob Hanlon <rob@citizencode.io>
2017-07-11 13:59:28 -0700

    remove commented code from contact info step

--- app/steps/introduction_contact_information.rb
+++ app/steps/introduction_contact_information.rb
@@ -1,45 +1,4 @@
 class IntroductionContactInformation < SimpleStep
-=begin
-  self.title = "Introduction"
-  self.subhead = "Tell us the best ways to reach you."
-
-  self.questions = {
-    phone_number: "What is the best phone number to reach you?",
-    accepts_text_messages: "May we send text messages to that phone number help you through the enrollment process?",
-    email: "What is your email address?",
-    street_address: "Address",
-    city: "City",
-    zip: "ZIP Code",
-    mailing_address_same_as_home_address: "Is this address the same as your home address?",
-  }
-
-  self.placeholders = {
-    phone_number: "Phone #",
-    email: "youremail@email.com",
-    street_address: "Street Address",
-    city: "City",
-    zip: "ZIP"
-  }
-
-  self.help_messages = {
-    email: "If you don’t have one or you don’t want to receive emails "\
-      "from us you can leave this blank.",
-    street_address: "This can be a street address or P.O. Box. If you don’t have a mailing address right now, that’s okay, just use ours:
-4800 Collins Rd, Lansing, 48924"
-  }
-
-  self.types = {
-    accepts_text_messages: :yes_no,
-    mailing_address_same_as_home_address: :yes_no
-  }
-
-  self.section_headers = {
-    phone_number: "Phone",
-    email: "Email",
-    street_address: "Where can we send you mail?"
-  }
-=end
-
   attr_accessor \
     :phone_number,
     :accepts_text_messages,
@@ -69,38 +28,6 @@ class IntroductionContactInformation < SimpleStep
     presence: { message: "Make sure to answer this question" }
 
   before_validation :strip_non_digits_from_phone_number
-
-=begin
-  def assign_from_app
-    assign_attributes(
-      phone_number: @app.phone_number,
-      accepts_text_messages: @app.accepts_text_messages,
-      street_address: @app.mailing_street,
-      city: @app.mailing_city,
-      zip: @app.mailing_zip,
-      mailing_address_same_as_home_address: @app
-        .mailing_address_same_as_home_address,
-      email: @app.email
-    )
-  end
-
-  def update_app!
-    @app.update!(
-      phone_number: phone_number,
-      accepts_text_messages: accepts_text_messages,
-      mailing_address_same_as_home_address: mailing_address_same_as_home_address,
-      mailing_street: street_address,
-      mailing_city: city,
-      mailing_zip: zip,
-      email: email
-    )
-
-    unless @app.welcome_sms_sent
-      Sms.new(@app).deliver_welcome_message
-      @app.update! welcome_sms_sent: true
-    end
-  end
-=end
 
   def strip_non_digits_from_phone_number
     self.phone_number = phone_number.remove(/[^\d]/)
