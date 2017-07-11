2a955f97c92d88dd431b07160fa43185b0ffc4a7
Rob Hanlon <rob@citizencode.io>
2017-07-11 16:11:08 -0700

    use @step because it is always needed

--- app/helpers/simple_step_helper.rb
+++ app/helpers/simple_step_helper.rb
@@ -1,6 +1,6 @@
 module SimpleStepHelper
-  def back_path(step)
-    previous = StepNavigation.new(step).previous
+  def back_path
+    previous = StepNavigation.new(@step).previous
     previous ? step_path(previous.to_param) : root_path
   end
 end
