8bf7880381d39ab1162c5391f14a4c8b91261ed4
Erik Hanson and Rob Hanlon <erik+rob@citizencode.io>
2017-07-10 17:54:25 -0700

    remove old comments

--- app/steps/introduction_introduce_yourself.rb
+++ app/steps/introduction_introduce_yourself.rb
@@ -1,21 +1,4 @@
 class IntroductionIntroduceYourself < SimpleStep
-=begin
-  self.icon = "hello"
-  self.title = "Introduction"
-  self.headline = "We're here to help."
-  self.subhead = "To start, please introduce yourself."
-
-  self.questions = {
-    first_name: "What is your first name?",
-    last_name: "What is your last name?",
-  }
-
-  self.placeholders = {
-    first_name: "First name",
-    last_name: "Last name",
-  }
-=end
-
   attr_accessor :first_name, :last_name
 
   validates :first_name,
@@ -23,15 +6,4 @@ class IntroductionIntroduceYourself < SimpleStep
 
   validates :last_name,
     presence: { message: "Make sure to provide a last name" }
-
-=begin
-  def assign_from_app
-    self.first_name = @app.applicant.first_name
-    self.last_name = @app.applicant.last_name
-  end
-
-  def update_app!
-    @app.applicant.update!(first_name: first_name, last_name: last_name)
-  end
-=end
 end
