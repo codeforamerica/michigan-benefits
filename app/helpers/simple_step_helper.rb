581ea888977a6a17a4f7865e94e2b0b259637172
Rob Hanlon <rob@citizencode.io>
2017-07-12 16:21:01 -0700

    extract questions helper

--- app/helpers/simple_step_helper.rb
+++ app/helpers/simple_step_helper.rb
@@ -4,4 +4,10 @@ module SimpleStepHelper
   def icon(name)
     haml_tag "div.step-section-header__icon.illustration.illustration--#{name}"
   end
+
+  def questions(f, questions)
+    questions.each do |question|
+      haml_concat render 'shared/question', f: f, **question
+    end
+  end
 end
