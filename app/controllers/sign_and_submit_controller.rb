39dbca108d882bafddcbe72b352f9271bee133f5
Rob Hanlon <rob@citizencode.io>
2017-07-19 15:24:14 -0700

    indentation fixes

--- app/controllers/sign_and_submit_controller.rb
+++ app/controllers/sign_and_submit_controller.rb
@@ -1,18 +1,16 @@
 # frozen_string_literal: true
 
 class SignAndSubmitController < StandardSimpleStepController
-
   def update
     @step = step_class.new(step_params)
 
     if @step.valid?
       current_app.update!(step_params)
-        FormMailer.submission(form: current_app.form).deliver_now
-        Sms.new(current_app).deliver_submission_message
+      FormMailer.submission(form: current_app.form).deliver_now
+      Sms.new(current_app).deliver_submission_message
       redirect_to(next_path)
     else
       render :edit
     end
-
   end
 end
