699070db3be713e54f47c07bd194c1f6c92a2ef6
Rob Hanlon <rob@citizencode.io>
2017-07-20 08:39:30 -0700

    remove unused foundation widgets

--- app/views/widgets/helpers.rb
+++ app/views/widgets/helpers.rb
@@ -1,15 +1,4 @@
 # frozen_string_literal: true
 
 module Views::Widgets::Helpers
-  def top_bar(title:)
-    widget Views::Widgets::TopBar, title: title
-  end
-
-  def basic_form_for(*form_for_params, title:, submit: nil, &block)
-    widget Views::Widgets::BasicFormFor, form_for_params: form_for_params, title: title, submit: submit, definition: block
-  end
-
-  def basic_table_for(enumerable, classes: nil, &block)
-    widget Views::Widgets::BasicTableFor, enumerable: enumerable, classes: classes, definition: block
-  end
 end
