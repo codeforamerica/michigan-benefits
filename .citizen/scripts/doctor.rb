0128d3393b8f41d3564474771f4f00e32cac3d0a
Jessie A. Young <jessie.a.young@gmail.com>
2017-07-20 15:10:24 -0700

    Update doctor script for world without envrc

--- .citizen/scripts/doctor.rb
+++ .citizen/scripts/doctor.rb
@@ -18,6 +18,36 @@ class Doctor < CitizenScripts::Doctor
   end
 
   def check_direnv_installed
-    #no-op
+    # no-op
+  end
+
+  def check_envrc_file_exists
+    # no-op
+  end
+
+  def check_db_exists
+    check \
+      name: "Development database exists",
+      command: "source .env && rails runner -e development 'ActiveRecord::Base.connection'",
+      remedy: command("rake db:setup")
+
+    check \
+      name: "Test database exists",
+      command: "source .env && rails runner -e test 'ActiveRecord::Base.connection'",
+      remedy: command("rake db:setup")
+  end
+
+  def check_db_migrated
+    check \
+      name: "DB is migrated",
+      command: "source .env && rails runner 'ActiveRecord::Migration.check_pending!'",
+      remedy: command("rake db:migrate db:test:prepare")
+  end
+
+  def check_env_file_exists
+    check \
+      name: ".env file exists (for 'heroku local')",
+      command: "stat .env",
+      remedy: command("cp .env.sample .env")
   end
 end
