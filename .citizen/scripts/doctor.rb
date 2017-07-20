00f89a801a21922762227c8cb6e8e2156e4c887a
Jessie A. Young <jessieay@users.noreply.github.com>
2017-07-20 15:26:54 -0700

    Updates to setup script (#7)
    * Now, running setup script does not result in any errors when running citizen
      doctor script.
    * Use `.env` rather than `.envrc`
    * Update doctor script for world without envrc
    * Add link to original script

--- .citizen/scripts/doctor.rb
+++ .citizen/scripts/doctor.rb
@@ -1,3 +1,5 @@
+# https://github.com/citizencode/citizen-scripts/blob/master/lib/citizen_scripts/doctor.rb
+
 class Doctor < CitizenScripts::Doctor
   def run_checks
     # Use built-in default checks, if still desired
@@ -15,5 +17,39 @@ class Doctor < CitizenScripts::Doctor
       command: "which pdftk",
       remedy: "http://www.pdflabs.com/tools/pdftk-server/"
     )
+  end
+
+  def check_direnv_installed
+    # no-op
+  end
+
+  def check_envrc_file_exists
+    # no-op
+  end
+
+  def check_db_exists
+    check \
+      name: "Development database exists",
+      command: "source .env && rails runner -e development 'ActiveRecord::Base.connection'",
+      remedy: command("rake db:setup")
+
+    check \
+      name: "Test database exists",
+      command: "source .env && rails runner -e test 'ActiveRecord::Base.connection'",
+      remedy: command("rake db:setup")
+  end
+
+  def check_db_migrated
+    check \
+      name: "DB is migrated",
+      command: "source .env && rails runner 'ActiveRecord::Migration.check_pending!'",
+      remedy: command("rake db:migrate db:test:prepare")
+  end
+
+  def check_env_file_exists
+    check \
+      name: ".env file exists (for 'heroku local')",
+      command: "stat .env",
+      remedy: command("cp .env.sample .env")
   end
 end
