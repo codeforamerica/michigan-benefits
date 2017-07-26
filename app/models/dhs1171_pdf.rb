09e5e700fa966e62f67c04c0a56fe92890ad2d74
Jessie A. Young <jessieay@users.noreply.github.com>
2017-07-25 17:59:41 -0700

    Add fillable DHS 1171 and fill in minimal data (#21)
    * Add fillable DHS 1171 and fill in minimal data
    
    **WHY**: MVP!

---
+++ app/models/dhs1171_pdf.rb
@@ -0,0 +1,41 @@
+class Dhs1171Pdf
+  FIELDS = [
+    "applying_for_food_assistance",
+    "birth_day",
+    "birth_month",
+    "birth_year",
+    "city",
+    "county",
+    "full_name",
+    "signature",
+    "signature_date",
+    "state",
+    "street_address",
+    "zip",
+  ].freeze
+
+  SOURCE_PDF = "DHS_1171.pdf".freeze
+
+  def initialize(client_data)
+    @client_data = client_data
+  end
+
+  def save(new_pdf_file_name)
+    check_for_invalid_fields
+    PdfForms.new.fill_form(SOURCE_PDF, new_pdf_file_name, client_data)
+  end
+
+  private
+
+  attr_reader :client_data, :source_pdf
+
+  def check_for_invalid_fields
+    if invalid_client_fields.any?
+      raise "Invalid fields passed in: #{invalid_client_fields}"
+    end
+  end
+
+  def invalid_client_fields
+    client_data.keys.map(&:to_s) - FIELDS
+  end
+end
