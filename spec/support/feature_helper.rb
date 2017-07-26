c55f60c22f7f4a1ad29c6e02ae4415e4399b3c73
Jessie A. Young <jessie.a.young@gmail.com>
2017-07-26 15:22:43 -0700

    More cleanup
    * Delete unused code / spec helpers / readmes

--- spec/support/feature_helper.rb
+++ spec/support/feature_helper.rb
@@ -1,36 +1,4 @@
 # frozen_string_literal: true
 
 module FeatureHelper
-  def xstep(title)
-    puts "PENDING STEP SKIPPED: #{title}" unless ENV["QUIET_TESTS"]
-  end
-
-  def step(title)
-    print title unless ENV["QUIET_TESTS"]
-    yield
-    puts unless ENV["QUIET_TESTS"]
-  end
-
-  def save_and_open_preview
-    file_preview_url = file_preview_url(host: "localhost:3000", file: save_page)
-    `open #{file_preview_url}`
-  end
-
-  def wut
-    save_and_open_preview
-  end
-
-  def flash
-    find(".flash").text
-  end
-
-  def login_as_member(member = nil)
-    member ||= create :member
-
-    visit root_path
-    click_on "Log In"
-    fill_in "Email", with: member.email
-    fill_in "Password", with: "password"
-    click_button "Continue"
-  end
 end
