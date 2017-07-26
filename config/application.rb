ab550c4c4ba844202ce87e1986719a052e2d4075
Jessie A. Young <jessieay@users.noreply.github.com>
2017-07-25 17:49:35 -0700

    Add rubocop to default Rake task (#20)
    **WHY**: otherwise, easy to forget to run before pushing.
    
    Minor: removed some unused `bin` stuff

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
 
 require_relative "boot"
-
 require "rails/all"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module MichiganPrototype
