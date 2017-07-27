743a0a065ad40437dca3c2671bde0ff5e9139984
Adarsh Pandit <adarshpandit@gmail.com>
2017-07-27 11:58:47 -0700

    test

--- spec/models/cover_sheet_pdf_spec.rb
+++ spec/models/cover_sheet_pdf_spec.rb
@@ -3,7 +3,9 @@ require "rails_helper"
 RSpec.describe CoverSheetPdf do
   describe "#save" do
     it "saves a new coversheet pdf file" do
-      expect(file).to be_present
+      file_name = "tmp/test_cover_sheet.pdf"
+
+      expect(File.exists?(file_name)).to be_truthy
     end
   end
 end
