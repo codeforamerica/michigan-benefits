74468193131c902789aa3116db77aa6e91935e16
Jessie A. Young <jessie.a.young@gmail.com>
2017-07-28 17:17:31 -0700

    Style forms the way that GCF does it
    * Using a formbuilder helper!

--- app/helpers/steps_helper.rb
+++ app/helpers/steps_helper.rb
@@ -5,38 +5,7 @@ module StepsHelper
     haml_tag "div.step-section-header__icon.illustration.illustration--#{name}"
   end
 
-  def questions(f, questions)
-    questions.each do |question|
-      haml_concat render "shared/question", f: f, **question
-    end
-  end
-
-  def header_name(household_member)
-    name = household_member.first_name.titleize
-    household_member.applicant? ? "#{name} (that’s you!)" : name
-  end
-
-  def fields_for_each_member(f, household_members)
-    household_members.each do |member|
-      haml_concat(
-        f.fields_for("household_members[]", member, hidden_field_id: true) do |ff|
-          yield ff, member
-        end,
-      )
-    end
-  end
-
   def data_md5(str)
     Digest::MD5.hexdigest(str ? str.squish : "")
-  end
-
-  def inconsistent_members(household_members)
-    household_members.with_inconsistent_income.map(&:name).to_sentence
-  end
-
-  def progress
-    step_navigation = StepNavigation.new(controller)
-    index = step_navigation.index
-    "#{index ? index + 1 : ''}/#{StepNavigation.steps.size}"
   end
 end
