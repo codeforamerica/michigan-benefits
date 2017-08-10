690242d40dd870b0b2bdecb9d94bd919adb4df0d
Jessie A. Young <jessie.a.young@gmail.com>
2017-08-09 17:03:09 -0700

    Rename app in config
    Prototype no more!

--- config/application.rb
+++ config/application.rb
@@ -5,16 +5,15 @@ require "rails/all"
 
 Bundler.require(*Rails.groups)
 
-module MichiganPrototype
+module MichiganBenefits
   class Application < Rails::Application
     config.action_controller.action_on_unpermitted_parameters = :raise
 
     # Project configuration
-    config.site_name = "Michigan Assistance Programs"
+    config.site_name = "Michigan Benefits"
     config.project_description = ""
     config.active_job.queue_adapter = :delayed_job
 
     config.autoload_paths << Rails.root.join("app/steps")
-    config.autoload_paths << Rails.root.join("app/questions")
   end
 end
