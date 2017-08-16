fe6daefacd7550a98b86bc404dc5c050f4c07a95
Jessie A. Young <jessie.a.young@gmail.com>
2017-08-16 11:40:40 -0700

    Add household "more info" questions per member
    * Ask about each person based on meta responses about household.

---
+++ app/steps/household_more_info_per_member.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class HouseholdMoreInfoPerMember < Step
+  step_attributes :members
+end
--- spec/support/feature_helper.rb
+++ spec/support/feature_helper.rb
@@ -11,6 +11,40 @@ module FeatureHelper
     )
   end
 
+  def answer_household_more_info_questions(js: true)
+    select_radio(
+      question: "Is each person a US citizen/national?",
+      answer: "Yes",
+    )
+    select_radio(
+      question: "Does anyone have a disability?",
+      answer: "No",
+    )
+    answer_pregnancy_question(js)
+    select_radio(
+      question: "Is anyone enrolled in college or vocational school?",
+      answer: "No",
+    )
+    select_radio(
+      question: "Is anyone temporarily living outside the home?",
+      answer: "No",
+    )
+  end
+
+  def answer_pregnancy_question(js)
+    if js == true
+      js_select_radio(
+        question: "Is anyone pregnant or has been pregnant recently?",
+        answer_id: "step_anyone_new_mom_false",
+      )
+    else
+      select_radio(
+        question: "Is anyone pregnant or has been pregnant recently?",
+        answer: "No",
+      )
+    end
+  end
+
   def select_radio(question:, answer:)
     within(find(:fieldset, text: question)) do
       choose answer
