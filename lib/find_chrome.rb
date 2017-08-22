0439e957cb2b57f7b082b43231770ef91eab5d5a
Joel Oliveira <jayroh@users.noreply.github.com>
2017-08-22 17:21:01 -0400

    Use headless chrome instead of poltergeist (#123)
    When our JS-enabled tests run we should use (headless) chrome and
    chromedriver as the driver. This removes the dependency of poltergeist
    but adds the need for chromedriver. As a result:
    
    1. Add the installation of chromedriver to our setup script
    2. Add the installation of chromedriver to circle.yml
    
    Because the platforms we test on install chrome(driver) in different
    locations this commit adds a `FindChrome` class that will locate chrome
    and chromedriver in either the most common locations, or a location we
    can specify with the ENV variables `CHROME_PATH` or `CHROMEDRIVER_PATH`.
    If they're not found - raise an exception.
    
    Default JS driver is `:headless_chrome` but can be changed to `:chrome`
    in `rails_helper.rb` if we want to watch those tests in a real chrome
    browser.
    
    Also:
    
    * Update feature_helper to work with this driver. For some reason
    capybara was complaining about the previous api usage - something about
    how that wasn't implemented. ¯\_(ツ)_/¯

---
+++ lib/find_chrome.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "mkmf"
+
+class FindChrome
+  CHROME_PATHS = [
+    "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+    "/opt/google/chrome/chrome",
+    ENV["CHROME_PATH"],
+    find_executable("chrome"),
+  ].compact.freeze
+
+  CHROMEDRIVER_PATHS = [
+    "/usr/local/bin/chromedriver",
+    ENV["CHROMEDRIVER_PATH"],
+    find_executable("chromedriver"),
+  ].compact.freeze
+
+  def self.binary
+    search(CHROME_PATHS, for_binary_named: "Chrome")
+  end
+
+  def self.driver
+    search(CHROMEDRIVER_PATHS, for_binary_named: "Chromedriver")
+  end
+
+  def self.search(paths, for_binary_named:)
+    found = paths.select { |path| File.executable? path }
+    return found.first if found.any?
+    raise "#{for_binary_named} not found on system"
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,31 +4,23 @@ ENV["RAILS_ENV"] ||= "test"
 require "spec_helper"
 require File.expand_path("../../config/environment", __FILE__)
 require "rspec/rails"
-
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
-
-headless_capybara = true
 require "capybara/rails"
 require "capybara/rspec"
+require "selenium/webdriver"
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
-if headless_capybara
-  require "capybara/poltergeist"
-  Capybara.javascript_driver = :poltergeist
-else
-  Capybara.register_driver :chrome do |app|
-    Capybara::Selenium::Driver.new(app, browser: :chrome)
-  end
-  Capybara.javascript_driver = :chrome
-end
+Capybara.javascript_driver = :headless_chrome # or `:chrome` for full browser
 
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = false
+
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
   end
+
   config.before(:each) do |example|
     DatabaseCleaner.strategy = if example.metadata[:js]
                                  :truncation
@@ -37,13 +29,13 @@ RSpec.configure do |config|
                                end
     DatabaseCleaner.start
   end
+
   config.after(:each) do
     DatabaseCleaner.clean
     FakeTwilioClient.clear!
   end
 
   config.infer_spec_type_from_file_location!
-
   config.include FeatureHelper, type: :feature
   config.include GenericHelper
 end
