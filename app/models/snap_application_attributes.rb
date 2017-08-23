0b2c931a07a71f4d774a79659a69fa5e113690c6
Jessie A. Young <jessieay@users.noreply.github.com>
2017-08-23 12:44:35 -0700

    Fill in income and expense fields (#124)
    * Yay PDFs

--- app/models/snap_application_attributes.rb
+++ app/models/snap_application_attributes.rb
@@ -50,6 +50,62 @@ class SnapApplicationAttributes
         boolean_to_checkbox(employed_household_members.any?),
       employed_household_members_no:
         boolean_to_checkbox(employed_household_members.empty?),
+      additional_income_yes:
+        boolean_to_checkbox(snap_application.additional_income.any?),
+      additional_income_no:
+        boolean_to_checkbox(snap_application.additional_income.empty?),
+      additional_income_social_security: additional_income?("social_security"),
+      additional_income_pension: additional_income?("pension"),
+      additional_income_unemployment:
+        additional_income?("unemployment_insurance"),
+      additional_income_ssi: additional_income?("ssi_or_disability"),
+      additional_income_workers_compensation:
+        additional_income?("workers_compensation"),
+      additional_income_child_support: additional_income?("child_support"),
+      additional_income_other: additional_income?("other"),
+      court_ordered_expenses_child_support:
+        court_ordered_expense?("child_support"),
+      court_ordered_expenses_alimony: court_ordered_expense?("alimony"),
+      court_ordered_expenses_yes:
+        boolean_to_checkbox(snap_application.court_ordered?),
+      court_ordered_expenses_no:
+        boolean_to_checkbox(!snap_application.court_ordered?),
+      monthly_court_ordered_expenses:
+        snap_application.monthly_court_ordered_expenses,
+      medical_expenses_yes:
+        boolean_to_checkbox(snap_application.medical?),
+      medical_expenses_no:
+        boolean_to_checkbox(!snap_application.medical?),
+      medical_expenses_health_insurance: medical_expense?("health_insurance"),
+      medical_expenses_prescriptions: medical_expense?("prescriptions"),
+      medical_expenses_dental: medical_expense?("dental"),
+      medical_expenses_transportation: medical_expense?("transportation"),
+      medical_expenses_hospital_bills: medical_expense?("hospital_bills"),
+      medical_expenses_other: medical_expense?("other"),
+      monthly_medical_expenses: snap_application.monthly_medical_expenses,
+      dependent_care_yes: boolean_to_checkbox(snap_application.dependent_care?),
+      dependent_care_no: boolean_to_checkbox(!snap_application.dependent_care?),
+      care_expenses_childcare: care_expense?("childcare"),
+      care_expenses_disabled: care_expense?("disabled_adult_care"),
+      monthly_care_expenses: snap_application.monthly_care_expenses,
+      rent_expense_yes:
+        boolean_to_checkbox(snap_application.rent_expense.present?),
+      rent_expense: snap_application.rent_expense,
+      property_tax_expense_yes:
+        boolean_to_checkbox(snap_application.property_tax_expense.present?),
+      annual_property_tax_expense: annual_property_tax_expense,
+      annual_insurance_expense: annual_insurance_expense,
+      insurance_expense_yes:
+        boolean_to_checkbox(snap_application.insurance_expense.present?),
+      utility_heat: boolean_to_checkbox(snap_application.utility_heat?),
+      utility_cooling: boolean_to_checkbox(snap_application.utility_cooling?),
+      utility_electricity:
+        boolean_to_checkbox(snap_application.utility_electrity?),
+      utility_water_sewer:
+        boolean_to_checkbox(snap_application.utility_water_sewer?),
+      utility_trash: boolean_to_checkbox(snap_application.utility_trash?),
+      utility_phone: boolean_to_checkbox(snap_application.utility_phone?),
+      utility_other: boolean_to_checkbox(snap_application.utility_other?),
     }
   end
 
@@ -117,5 +173,41 @@ class SnapApplicationAttributes
   def employed_household_members
     @_employed_household_members ||=
       snap_application.members.where(employment_status: "employed")
+  end
+
+  def additional_income?(type)
+    boolean_to_checkbox(
+      snap_application.additional_income.include?(type),
+    )
+  end
+
+  def medical_expense?(type)
+    boolean_to_checkbox(
+      snap_application.medical_expenses.include?(type),
+    )
+  end
+
+  def court_ordered_expense?(type)
+    boolean_to_checkbox(
+      snap_application.court_ordered_expenses.include?(type),
+    )
+  end
+
+  def care_expense?(type)
+    boolean_to_checkbox(
+      snap_application.care_expenses.include?(type),
+    )
+  end
+
+  def annual_property_tax_expense
+    if snap_application.property_tax_expense
+      snap_application.property_tax_expense * 12
+    end
+  end
+
+  def annual_insurance_expense
+    if snap_application.insurance_expense
+      snap_application.insurance_expense * 12
+    end
   end
 end
