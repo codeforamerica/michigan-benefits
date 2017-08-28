e29df7372747a8235c413ebda498b319374b1c01
Jessie A. Young <jessie.a.young@gmail.com>
2017-08-28 09:27:06 -0700

    Update header style on newest page
    * Was created / merged after FE audit on Friday

--- app/helpers/steps_helper.rb
+++ app/helpers/steps_helper.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
 module StepsHelper
-  def icon(name)
-    content_tag(
-      :div,
-      class: "step-section-header__icon.illustration.illustration--#{name}",
-    )
-  end
-
   def header_name(household_member)
     name = household_member.first_name.titleize
     household_member.primary_member? ? "#{name} (that’s you!)" : name
