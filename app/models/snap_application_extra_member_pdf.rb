141180ce63d7bfa00e11383a0e559248f1aa7049
Jessie A. Young <jessieay@users.noreply.github.com>
2017-08-29 11:04:53 -0700

    Add 3 logo footer to PDF (#150)
    * This adds the footer to each page of the PDF, no matter the length
    * Using a bounding box for text so that the footer image and text do not
    overlap

--- app/models/snap_application_extra_member_pdf.rb
+++ app/models/snap_application_extra_member_pdf.rb
@@ -9,25 +9,30 @@ class SnapApplicationExtraMemberPdf
 
   def completed_file
     pdf = Prawn::Document.new
-    pdf.font("Helvetica", size: 16) do
-      pdf.text("Section C Continued")
-    end
-    pdf.move_down 10
 
-    members.each do |member|
-      attributes = SnapApplicationExtraMemberAttributes.new(member: member)
-
-      pdf.font("Helvetica", size: 14) do
-        pdf.text(attributes.title)
-      end
-
-      attributes.to_a.each do |attribute|
-        pdf.text(attribute)
+    pdf.bounding_box([40, pdf.cursor - 40], width: 500, height: 600) do
+      pdf.font("Helvetica", size: 16) do
+        pdf.text("Section C Continued")
       end
 
       pdf.move_down 10
+
+      members.each do |member|
+        attributes = SnapApplicationExtraMemberAttributes.new(member: member)
+
+        pdf.font("Helvetica", size: 14) do
+          pdf.text(attributes.title)
+        end
+
+        attributes.to_a.each do |attribute|
+          pdf.text(attribute)
+        end
+
+        pdf.move_down 10
+      end
     end
 
+    add_footer(pdf)
     pdf.render_file(file_path)
     file_path
   end
@@ -43,5 +48,21 @@ class SnapApplicationExtraMemberPdf
   def filename
     @filename ||=
       "#{SecureRandom.hex}#{Time.now.strftime('%Y%m%d%H%M%S%L')}"
+  end
+
+  def add_footer(pdf)
+    pdf.page_count.times do |i|
+      pdf.go_to_page i
+      pdf.image(
+        footer_filename,
+        position: :center,
+        vposition: :bottom,
+        width: 500,
+      )
+    end
+  end
+
+  def footer_filename
+    "#{Rails.root}/public/pdf_footer.jpg"
   end
 end
