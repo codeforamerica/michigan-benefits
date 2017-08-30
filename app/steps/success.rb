2240865f5559e2c3e8ded0609e6d9b8ba4178d8d
Jessie A. Young <jessie.a.young@gmail.com>
2017-08-30 11:06:00 -0700

    Use signed_at instead of faxed_at

--- app/steps/success.rb
+++ app/steps/success.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 class Success < Step
-  step_attributes :email, :faxed_at
+  step_attributes :email, :signed_at
 
   validates :email,
     presence: { message: "Make sure to provide an email address" }
 
   def submitted_date
-    if faxed_at.present?
-      faxed_at.strftime("%m-%d-%Y")
+    if signed_at.present?
+      signed_at.strftime("%m-%d-%Y")
     end
   end
 end
--- spec/steps/success_spec.rb
+++ spec/steps/success_spec.rb
@@ -2,17 +2,17 @@ require "rails_helper"
 
 RSpec.describe PersonalDetail do
   describe "#submitted_date" do
-    context "faxed_at present" do
+    context "signed_at present" do
       it "returns a formatted date" do
-        step = Success.new(faxed_at: DateTime.parse("January 30, 1900"))
+        step = Success.new(signed_at: DateTime.parse("January 30, 1900"))
 
         expect(step.submitted_date).to eq "01-30-1900"
       end
     end
 
-    context "faxed_at not present" do
+    context "signed_at not present" do
       it "returns nil" do
-        step = Success.new(faxed_at: nil)
+        step = Success.new(signed_at: nil)
 
         expect(step.submitted_date).to eq nil
       end
