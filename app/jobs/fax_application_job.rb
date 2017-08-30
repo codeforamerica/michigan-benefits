af03229566b95fc07fcb93b668b5cff06f4035ff
Zee Spencer@Cohere <zee@wecohere.com>
2017-08-30 17:37:23 +0000

    Simplify tests and logic in the fax application job

--- app/jobs/fax_application_job.rb
+++ app/jobs/fax_application_job.rb
@@ -2,35 +2,24 @@ class FaxApplicationJob < ApplicationJob
   def perform(snap_application_id:)
     @snap_application = SnapApplication.find(snap_application_id)
     return if snap_application.faxed?
-    fax_pdf
+    Fax.send_fax(
+      number: fax_recipient.number,
+      file: snap_application.pdf.path,
+      recipient: fax_recipient.name,
+    )
     snap_application.update(faxed_at: Time.zone.now)
   ensure
-    pdf.try(:close)
-    pdf.try(:unlink)
+    snap_application.pdf.try(:close)
+    snap_application.pdf.try(:unlink)
   end
 
   private
 
   attr_reader :snap_application
-  delegate :pdf, :residential_address, to: :snap_application
-
-  def fax_pdf
-    Fax.send_fax(
-      number: fax_number,
-      file: pdf.path,
-      recipient: fax_recipient_name,
-    )
-  end
 
   def fax_recipient
-    FaxRecipient.new(residential_address: residential_address)
-  end
-
-  def fax_recipient_name
-    fax_recipient.name
-  end
-
-  def fax_number
-    fax_recipient.number
+    @fax_recipient ||=
+      FaxRecipient.new(residential_address:
+                         snap_application.residential_address)
   end
 end
