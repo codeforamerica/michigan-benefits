1f246c0cd9189227dc13240c23ed2bbe3bbff730
Jessie A. Young <jessieay@users.noreply.github.com>
2017-08-30 11:13:51 -0700

    Update style of success page (#160)
    - Iconography/Illustration at the top
    - "Blue Text" that says "Congratulations"
    - Change the headline text
    - Include subheader text that gives the client the date submitted.
    - Include the specification of which program was submitted, using the icons that represent the FAP program (found on the homepage as well).

---
+++ spec/steps/success_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe PersonalDetail do
+  describe "#submitted_date" do
+    context "signed_at present" do
+      it "returns a formatted date" do
+        step = Success.new(signed_at: DateTime.parse("January 30, 1900"))
+
+        expect(step.submitted_date).to eq "01-30-1900"
+      end
+    end
+
+    context "signed_at not present" do
+      it "returns nil" do
+        step = Success.new(signed_at: nil)
+
+        expect(step.submitted_date).to eq nil
+      end
+    end
+  end
+end
