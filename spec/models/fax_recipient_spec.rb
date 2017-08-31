7e34f0931991008e5ed9f082eb5476e61ad9d3e0
Zee <zee@wecohere.com>
2017-08-31 14:05:11 -0700

    Fallback office is clio, fix unions number (#163)

--- spec/models/fax_recipient_spec.rb
+++ spec/models/fax_recipient_spec.rb
@@ -36,14 +36,14 @@ RSpec.describe FaxRecipient do
         residential_address = double(zip: "48411")
         fax_recipient = described_class.new(residential_address:
                                               residential_address)
-        expect(fax_recipient.number).to eql "+18107602021"
+        expect(fax_recipient.number).to eql "+18107607372"
       end
 
-      it "falls back to the union office" do
+      it "falls back to the clio office" do
         residential_address = double(zip: "123456")
         fax_recipient = described_class.new(residential_address:
                                               residential_address)
-        expect(fax_recipient.number).to eql "+18107602021"
+        expect(fax_recipient.number).to eql "+18107602310"
       end
     end
   end
