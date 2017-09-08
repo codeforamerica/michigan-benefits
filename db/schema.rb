a80922e27fec5e4bcd081158267247dbfb94b4bc
Jessie A. Young <jessieay@users.noreply.github.com>
2017-09-08 11:51:07 -0700

    Add "contact preference" step (#191)
    * SMS or Email
    * Removes "interview preference" step for now since we are not sending
    that data anywhere.
    * Follow up to this will be a page that confirms someone's phone number
    and email address based on what the opt into
    * Follow up will also be to actually send the email / SMS confirms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170829193351) do
+ActiveRecord::Schema.define(version: 20170908174732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -143,6 +143,7 @@ ActiveRecord::Schema.define(version: 20170829193351) do
     t.string "interview_preference"
     t.text "additional_information"
     t.datetime "faxed_at"
+    t.boolean "email_subscribed"
   end
 
   add_foreign_key "driver_applications", "snap_applications"
