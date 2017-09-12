1723e012de43fec4082beedbb063be8e2b0ce18e
Joel Oliveira <joel.oliveira@gmail.com>
2017-09-12 17:02:36 -0400

    Add utility and bills pages (w/summary pages)

---
+++ lib/mi_bridges/driver/housing_utility_bills_page.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+module MiBridges
+  class Driver
+    class HousingUtilityBillsPage < BasePage
+      delegate(
+        :primary_member,
+        to: :snap_application,
+      )
+
+      delegate :first_name, to: :primary_member
+
+      def setup; end
+
+      def fill_in_required_fields
+        check_housing_bills
+        check_utility_bills
+        check_room_and_meals
+        select_home_heating_payment
+      end
+
+      def continue
+        click_on "Next"
+      end
+
+      private
+
+      def check_housing_bills
+        check_in_section "starHousingBills", for: first_name
+      end
+
+      def check_utility_bills
+        check_in_section "starUtilityBills", for: first_name
+      end
+
+      def check_room_and_meals
+        check_in_section "starRoomandMeals"
+      end
+
+      def select_home_heating_payment
+        check_in_section "heatingResponseGroup", if: true, for: "No"
+        check_in_section "meapReceivedGroup", if: true, for: "No"
+      end
+    end
+  end
+end
