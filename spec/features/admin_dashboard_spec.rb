a76db26ca0b65a325a13c2947dd05371d1a7bd1b
Zee Spencer <zee@zincma.de>
2017-09-12 13:16:53 -0700

    Make admin feature spec faster and greedier

--- spec/features/admin_dashboard_spec.rb
+++ spec/features/admin_dashboard_spec.rb
@@ -2,14 +2,14 @@ require "rails_helper"
 RSpec.feature "Submit application with minimal information" do
   scenario "The stats are accurate", javascript: true do
     page.driver.browser.current_session.basic_authorize("admin", "password")
-    5.times { create(:snap_application, signed_at: nil) }
-    5.times { create(:snap_application, :emailed, signed_at: nil) }
-    10.times { create(:snap_application, signed_at: 5.days.ago) }
-    5.times { create(:snap_application, :faxed, signed_at: 5.days.ago) }
+    10.times { create(:snap_application, signed_at: nil) }
+    1.times { create(:snap_application, :emailed, signed_at: nil) }
+    9.times { create(:snap_application, signed_at: 5.days.ago) }
+    1.times { create(:snap_application, :faxed, signed_at: 5.days.ago) }
     visit admin_root_path
-    expect(page).to have_content "25 Created"
-    expect(page).to have_content "15 Signed"
-    expect(page).to have_content "5 Emailed"
-    expect(page).to have_content "5 Faxed"
+    expect(page).to have_content "21 Created"
+    expect(page).to have_content "10 Signed (47.6%)"
+    expect(page).to have_content "1 Emailed (4.8%)"
+    expect(page).to have_content "1 Faxed (4.8%)"
   end
 end
