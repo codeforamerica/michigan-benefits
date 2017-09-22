7f324672217d1d6440b722a64031625a18c08c3d
Jessie A. Young <jessieay@users.noreply.github.com>
2017-09-22 11:19:26 -0700

    Extract relationship/ buy food with sections into classes (#268)
    **WHY**: There is a lot of logic here. Better to encapsulate in a
    service obj.

--- lib/mi_bridges/driver/relationship_information_page.rb
+++ lib/mi_bridges/driver/relationship_information_page.rb
@@ -12,8 +12,14 @@ module MiBridges
 
       def fill_in_required_fields
         second_members.each do |member|
-          fill_in_relationship(member)
-          buy_food_with(member)
+          MiBridges::Driver::Services::SubmitRelationship.new(
+            first_member: first_member,
+            second_member: member,
+          ).run
+          MiBridges::Driver::Services::SubmitBuyFoodWith.new(
+            first_member: first_member,
+            second_member: member,
+          ).run
         end
       end
 
@@ -24,42 +30,6 @@ module MiBridges
       private
 
       attr_reader :first_member, :second_members
-
-      def fill_in_relationship(member)
-        selector = find(:xpath, relationship_xpath(member))
-        selector.select(mi_bridges_relationship(member))
-      end
-
-      def relationship_xpath(member)
-        second = member.mi_bridges_formatted_name
-        "//*[@title=\"* #{first_member_name}\'s Relationship to #{second}\"]"
-      end
-
-      def buy_food_with(member)
-        buy_food_with_section =
-          find("fieldset", text: buy_food_with_label(member))
-
-        within buy_food_with_section do
-          find(:xpath, ".//*[@title='#{buy_food_with_option(member)}']").click
-        end
-      end
-
-      def buy_food_with_label(member)
-        second = member.mi_bridges_formatted_name
-        "Does #{first_member_name} usually buy and fix food with #{second}?"
-      end
-
-      def buy_food_with_option(member)
-        if member.buy_food_with?
-          "Yes"
-        else
-          "No"
-        end
-      end
-
-      def first_member_name
-        first_member.mi_bridges_formatted_name
-      end
 
       def find_first_member
         name = page.first(:xpath, first_member_name_label).text
@@ -85,47 +55,6 @@ module MiBridges
         snap_application.members.detect do |member|
           member.mi_bridges_formatted_name == name
         end
-      end
-
-      def mi_bridges_relationship(member)
-        if first_member == snap_application.primary_member
-          map_member_relationship_to_mi_bridges(member)
-        else
-          "is Related to another way to"
-        end
-      end
-
-      def map_member_relationship_to_mi_bridges(member)
-        relationship = member.relationship || "Other"
-        sex = first_member.sex
-        mapping = send("#{sex}_relationship_mapping".to_sym).
-          merge(other_relationship_mapping)
-        mapping[relationship]
-      end
-
-      def female_relationship_mapping
-        {
-          "Child" => "is the Mother of",
-          "Parent" => "is the Daughter of",
-          "Sibling" => "is the Sister of",
-          "Spouse" => "is the Wife of",
-        }
-      end
-
-      def male_relationship_mapping
-        {
-          "Child" => "is the Father of",
-          "Parent" => "is the Son of",
-          "Sibling" => "is the Brother of",
-          "Spouse" => "is the Husband of",
-        }
-      end
-
-      def other_relationship_mapping
-        {
-          "Roommate" => "is Related to another way to",
-          "Other" => "is Related to another way to",
-        }
       end
     end
   end
