c8b8d7b9e13fb9e21fc76a84cd66a6d1a7882c6d
Jessie A. Young <jessieay@users.noreply.github.com>
2017-09-27 12:03:39 -0700

    Validate employer name for employed members (#282)
    **WHY**: This is a required field in MI Bridges, so we need to collect
    the info in order to drive applications successfully.

--- spec/steps/income_details_per_member_spec.rb
+++ spec/steps/income_details_per_member_spec.rb
@@ -1,17 +1,28 @@
 require "rails_helper"
 
 RSpec.describe IncomeDetailsPerMember do
-  describe "#working_members" do
-    it "returns members who are employed or self employed" do
-      working = create(:member, employment_status: "employed")
-      not_working = create(:member, employment_status: "not_employed")
-      self_employed = create(:member, employment_status: "self_employed")
-      members = [working, not_working, self_employed]
+  describe "Validations" do
+    it "validates employer name on employed members" do
+      working = create(
+        :member,
+        employment_status: "employed",
+        employed_employer_name: nil,
+      )
+      self_employed = create(
+        :member,
+        employment_status: "self_employed",
+        employed_employer_name: nil,
+      )
+      members = [working, self_employed]
       snap_application = create(:snap_application, members: members)
 
       step = IncomeDetailsPerMember.new(members: snap_application.members)
+      step.valid?
 
-      expect(step.working_members).to match_array([working, self_employed])
+      expect(step.members.last.errors[:employed_employer_name]).
+        to eq([])
+      expect(step.members.first.errors[:employed_employer_name]).
+        to eq(["Make sure to answer this question"])
     end
   end
 end
