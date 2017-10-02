31a93f8f8c6183f016a26919d4b2d03195a79985
Joel Oliveira <joel.oliveira@gmail.com>
2017-10-02 15:00:27 -0400

    Check that "From" header in emails have a name
    When emails are sent to an office or a client, the "FROM" field in the
    email should have a more approachable value instead of just
    "hello@somedomain.com".
    
    This commit changes the FROM header and adds some tests to accompany the
    resulting emails' headers, asserting their validity.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,17 +1,21 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: "hello@#{ENV['EMAIL_DOMAIN']}"
   layout "mailer"
 
   def snap_application_notification(file_name:, recipient_email:)
     attachments["snap_application.pdf"] = File.read(file_name)
-    mail to: recipient_email, subject: "Your SNAP application"
+    mail(
+      from: %("Michigan Benefits" <hello@#{ENV['EMAIL_DOMAIN']}>),
+      to: recipient_email,
+      subject: "Your SNAP application",
+    )
   end
 
   def office_application_notification(file_name:, recipient_email:)
     attachments["snap_application.pdf"] = File.read(file_name)
     mail(
+      from: %("Michigan Benefits" <hello@#{ENV['EMAIL_DOMAIN']}>),
       to: recipient_email,
       subject: "A new 1171 from the Digital Assister has been submitted!",
     )
