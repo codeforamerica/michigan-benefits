a412d1e99bdf6b7504322d295a3e8a1f84f43e33
Jessie A. Young <jessieay@users.noreply.github.com>
2017-10-04 12:34:29 -0700

    Use different email subject/body when client in office (#302)
    **WHY**: to alert office staff

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -12,12 +12,37 @@ class ApplicationMailer < ActionMailer::Base
     )
   end
 
-  def office_application_notification(file_name:, recipient_email:)
+  def office_application_notification(
+    file_name:,
+    recipient_email:,
+    office_location: nil
+  )
     attachments["snap_application.pdf"] = File.read(file_name)
+    @office_location = office_location
+
     mail(
       from: %("Michigan Benefits" <hello@#{ENV['EMAIL_DOMAIN']}>),
       to: recipient_email,
-      subject: "A new 1171 from the Digital Assister has been submitted!",
+      subject: subject(office_location),
+      template_name: template_name(office_location),
     )
+  end
+
+  private
+
+  def subject(office_location)
+    if office_location.present?
+      "A new 1171 from someone in the lobby has been submitted!"
+    else
+      "A new 1171 from someone online has been submitted!"
+    end
+  end
+
+  def template_name(office_location)
+    if office_location.present?
+      "in_office_application_notification"
+    else
+      "office_application_notification"
+    end
   end
 end
--- spec/mailers/application_mailer_spec.rb
+++ spec/mailers/application_mailer_spec.rb
@@ -21,21 +21,55 @@ RSpec.describe ApplicationMailer do
   end
 
   describe ".office_application_notification" do
-    it "sets the correct headers" do
-      with_modified_env EMAIL_DOMAIN: "example.com" do
-        email = ApplicationMailer.office_application_notification(
-          file_name: "#{Rails.root}/spec/fixtures/image.jpg",
-          recipient_email: "user@example.com",
-        )
-        from_header = email.header.select do |header|
-          header.name == "From"
-        end.first.value
+    context "office_location not present" do
+      it "sets the correct headers" do
+        with_modified_env EMAIL_DOMAIN: "example.com" do
+          email = ApplicationMailer.office_application_notification(
+            file_name: "#{Rails.root}/spec/fixtures/image.jpg",
+            recipient_email: "user@example.com",
+          )
+          from_header = email.header.select do |header|
+            header.name == "From"
+          end.first.value
 
-        expect(from_header).to eq %("Michigan Benefits" <hello@example.com>)
-        expect(email.from).to eq(["hello@example.com"])
-        expect(email.to).to eq(["user@example.com"])
-        expect(email.subject).
-          to eq("A new 1171 from the Digital Assister has been submitted!")
+          expect(from_header).to eq %("Michigan Benefits" <hello@example.com>)
+          expect(email.from).to eq(["hello@example.com"])
+          expect(email.to).to eq(["user@example.com"])
+          expect(email.subject).to eq(
+            "A new 1171 from someone online has been submitted!",
+          )
+          expect(email.body.encoded).not_to include(
+            "by a client in your office lobby",
+          )
+        end
+      end
+    end
+
+    context "office_location present" do
+      it "sets the correct values" do
+        with_modified_env EMAIL_DOMAIN: "example.com" do
+          email = ApplicationMailer.office_application_notification(
+            file_name: "#{Rails.root}/spec/fixtures/image.jpg",
+            recipient_email: "user@example.com",
+            office_location: "union",
+          )
+          from_header = email.header.select do |header|
+            header.name == "From"
+          end.first.value
+
+          expect(from_header).to eq %("Michigan Benefits" <hello@example.com>)
+          expect(email.from).to eq(["hello@example.com"])
+          expect(email.to).to eq(["user@example.com"])
+          expect(email.subject).to eq(
+            "A new 1171 from someone in the lobby has been submitted!",
+          )
+          expect(email.body.encoded).to include(
+            "Union",
+          )
+          expect(email.body.encoded).to include(
+            "by a client in your office lobby",
+          )
+        end
       end
     end
   end
