badcbd494841ecb36e5354151a21819103e8ab22
Jessie A. Young <jessie.a.young@gmail.com>
2017-10-10 12:34:14 -0700

    Autoload services, too

--- config/initializers/extensions.rb
+++ config/initializers/extensions.rb
@@ -2,6 +2,10 @@ Dir["#{Rails.root}/lib/mi_bridges/*.rb"].each do |file|
   require file
 end
 
+Dir["#{Rails.root}/lib/mi_bridges/driver/services/*.rb"].each do |file|
+  require file
+end
+
 Dir["#{Rails.root}/lib/mi_bridges/driver/*.rb"].each do |file|
   require file
 end
