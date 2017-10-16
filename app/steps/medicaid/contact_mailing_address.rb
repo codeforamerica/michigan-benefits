dff33ae488c3e1b0c1ece175e2ba95a198400ffd
Jessie A. Young <jessieay@users.noreply.github.com>
2017-10-16 11:01:32 -0700

    Validate address forms for Medicaid flow (#350)

--- app/steps/medicaid/contact_mailing_address.rb
+++ app/steps/medicaid/contact_mailing_address.rb
@@ -7,5 +7,14 @@ module Medicaid
       :mailing_city,
       :mailing_zip,
     )
+
+    validates :mailing_street_address,
+      presence: { message: "Make sure to provide a street address" }
+
+    validates :mailing_city,
+      presence: { message: "Make sure to provide a city" }
+
+    validates :mailing_zip,
+      length: { is: 5, message: "Make sure your ZIP code is 5 digits long" }
   end
 end
---
+++ spec/steps/medicaid/contact_home_address_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.describe Medicaid::ContactHomeAddress do
+  describe "Validations" do
+    it "validates presence of address fields" do
+      valid_address = Medicaid::ContactHomeAddress.new(
+        residential_street_address: "123 Main St.",
+        residential_city: "Flint",
+        residential_zip: "12345",
+      )
+
+      invalid_nil_address = Medicaid::ContactHomeAddress.new(
+        residential_street_address: nil,
+        residential_city: nil,
+        residential_zip: nil,
+      )
+
+      invalid_blank_address = Medicaid::ContactHomeAddress.new(
+        residential_street_address: "",
+        residential_city: "",
+        residential_zip: "",
+      )
+
+      expect(valid_address).to be_valid
+      expect(invalid_nil_address).not_to be_valid
+      expect(invalid_blank_address).not_to be_valid
+    end
+  end
+end
