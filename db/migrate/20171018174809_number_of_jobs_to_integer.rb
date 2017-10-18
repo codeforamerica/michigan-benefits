c38c51e3adb4ead3c572445282731cfe6be9316a
Jessie A. Young <jessie.a.young@gmail.com>
2017-10-18 14:04:55 -0700

    Newlines

--- db/migrate/20171018174809_number_of_jobs_to_integer.rb
+++ db/migrate/20171018174809_number_of_jobs_to_integer.rb
@@ -7,9 +7,11 @@ class NumberOfJobsToInteger < ActiveRecord::Migration[5.1]
         UPDATE medicaid_applications
         SET new_number_of_jobs=1
         WHERE number_of_jobs='1 job';
+
         UPDATE medicaid_applications
         SET new_number_of_jobs=2
         WHERE number_of_jobs='2 jobs';
+
         UPDATE medicaid_applications
         SET new_number_of_jobs=3
         WHERE number_of_jobs='3 or more jobs';
