5f8b29a9387e8850c8a832966dc970f3078c5739
Christa Hartsock <christa@codeforamerica.org>
2017-10-19 15:02:01 -0700

    Convert all timestamps in admin dashboard to EST.
    Customizes a partial for administrate that converts and formats any
    DateTime fields on detail pages to the same way they're shown elsewhere
    in the app for SNAP applications (using SnapApplication#signed_at_est
    as guide).
    
    Does not convert Date objects on index pages to EST.

--- spec/features/admin_dashboard_spec.rb
+++ spec/features/admin_dashboard_spec.rb
@@ -35,4 +35,23 @@ RSpec.feature "Submit application with minimal information" do
     expect(faxes.count).to eq 1
     expect(emails.count).to eq 1
   end
+
+  scenario "application detail timestamps are converted to Eastern timezone", javascript: true do
+    date = DateTime.new(2017,4,5,6,30)
+
+    application = create(:snap_application,
+           created_at: date,
+           updated_at: date + 1.hour,
+           email: "person@example.com",
+           signed_at: date + 2.hours)
+
+    visit admin_root_path
+    click_link_or_button "person@example.com"
+
+    expect(page).to have_content("Snap Application ##{application.id}")
+
+    expect(page).to have_content("Created At 04/05/2017 at 02:30AM EDT")
+    expect(page).to have_content("Updated At 04/05/2017 at 03:30AM EDT")
+    expect(page).to have_content("Signed At 04/05/2017 at 04:30AM EDT")
+  end
 end
