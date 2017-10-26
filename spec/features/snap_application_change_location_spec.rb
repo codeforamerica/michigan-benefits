fa723758b2d915716e23b91faee9f193dc2ed0f0
Jessie A. Young <jessie.a.young@gmail.com>
2017-10-26 11:03:57 -0700

    Enable SNAP clients to change office location
    **WHY**:
    - this surfaces the office location after the "Introduction" section of
    the SNAP application flow
    - if the office location is not what is expected, it can be changed.
    - this is useful when someone is applying in an MDHHS office but their
    address maps to a different location. We have office-specific landing
    pages to override this, but sometimes people forget to use those and
    they should be able to correct the location before submission.

---
+++ spec/features/snap_application_change_location_spec.rb
@@ -0,0 +1,44 @@
+require "rails_helper"
+
+RSpec.feature "Submit snap application" do
+  scenario "change office location" do
+    visit root_path
+    within(".slab--hero") { click_on "Apply now" }
+
+    on_page "Introduction" do
+      fill_in_name_and_birthday
+      click_on "Continue"
+    end
+
+    fill_in "What is the best phone number to reach you?", with: "2222222222"
+    click_on "Continue"
+
+    fill_in "Address", with: "123 Main St"
+    fill_in "City", with: "Flint"
+    fill_in "ZIP code", with: "48411"
+    select_address_same_as_home_address
+    click_on "Continue"
+
+    within(".form-card__content") do
+      expect(page).to have_content("Union")
+    end
+
+    on_page "Introduction Complete" do
+      click_on "here"
+    end
+
+    on_page "Office Submission" do
+      expect(page).to have_content(
+        "Where would you like to submit your application?",
+      )
+      choose "Clio Road"
+      click_on "Continue"
+    end
+
+    on_page "Introduction Complete" do
+      within(".form-card__content") do
+        expect(page).to have_content("Clio")
+      end
+    end
+  end
+end
