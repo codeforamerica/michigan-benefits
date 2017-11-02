5d52c531afde05616f49365ca4e0018c44b8dab2
Joel Oliveira <joel.oliveira@gmail.com>
2017-11-02 16:35:00 -0400

    Change order of paperwork + success pages
    [Finishes #152423848]
    
    Add an interstitial "paperwork guide" page per Tracker story.

--- spec/controllers/medicaid/sign_and_submit_controller_spec.rb
+++ spec/controllers/medicaid/sign_and_submit_controller_spec.rb
@@ -2,9 +2,9 @@ require "rails_helper"
 
 RSpec.describe Medicaid::SignAndSubmitController do
   describe "#next_path" do
-    it "is the insurance current path" do
+    it "is the paperwork guide path" do
       expect(subject.next_path).to eq(
-        "/steps/medicaid/confirmation",
+        "/steps/medicaid/paperwork-guide",
       )
     end
   end
