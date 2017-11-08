06ce1045f44e522a50d81672c0124be4803e01ef
Joel Oliveira <joel.oliveira@gmail.com>
2017-11-08 17:44:19 -0500

    Clean up amounts_income_controller
    Move a bunch of private methods into members or medicaid_application.

--- app/controllers/medicaid/amounts_income_controller.rb
+++ app/controllers/medicaid/amounts_income_controller.rb
@@ -11,7 +11,6 @@ module Medicaid
 
       if @step.valid?
         current_member.update!(step_params)
-        after_successful_update_hook
         redirect_to(next_path)
       else
         render :edit
@@ -39,48 +38,10 @@ module Medicaid
         first
     end
 
-    def employed_number_of_jobs
-      current_application.primary_member.employed_number_of_jobs
-    end
-
     def skip?
-      no_one_with_income? || member_has_no_income?
-    end
-
-    def no_one_with_income?
-      no_one_employed? &&
-        no_one_self_employed? &&
-        no_one_with_other_income?
-    end
-
-    def member_has_no_income?
-      member_not_employed? &&
-        member_not_self_employed? &&
-        member_not_receiving_unemployment?
-    end
-
-    def no_one_employed?
-      !current_application&.anyone_employed?
-    end
-
-    def no_one_self_employed?
-      !current_application&.anyone_self_employed?
-    end
-
-    def no_one_with_other_income?
-      !current_application&.anyone_other_income?
-    end
-
-    def member_not_employed?
-      !current_member&.employed?
-    end
-
-    def member_not_self_employed?
-      !current_member&.self_employed?
-    end
-
-    def member_not_receiving_unemployment?
-      !current_member&.unemployment_income?
+      current_application.no_one_with_income? ||
+        current_member.nil? ||
+        current_member&.not_receiving_income?
     end
   end
 end
