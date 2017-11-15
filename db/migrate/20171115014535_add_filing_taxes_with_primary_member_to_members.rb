582692b489c2527dffbd41a9dc0d5d697259dbad
Jessie A. Young <jessie.a.young@gmail.com>
2017-11-15 08:52:04 -0800

    Fix migration
    * This migration was accidentally updated incorrectly after it was
    migrated but before it was merged. This migration is currently blocking
    deploys to staging because it is an invalid migration.

--- db/migrate/20171115014535_add_filing_taxes_with_primary_member_to_members.rb
+++ db/migrate/20171115014535_add_filing_taxes_with_primary_member_to_members.rb
@@ -1,5 +1,5 @@
 class AddFilingTaxesWithPrimaryMemberToMembers < ActiveRecord::Migration[5.1]
   def change
-    add_column :members, :filing_taxes_with_primary_member
+    add_column :members, :filing_taxes_with_primary_member, :boolean
   end
 end
