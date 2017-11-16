7425d9f37d0775fd06a060c6d90e513aebf76d55
Jessie A. Young <jessie.a.young@gmail.com>
2017-11-16 14:02:02 -0800

    Update all members to if "everyone a citizen"
    * We were updating for a single member but not in a multi-member context
    * For most questions, if the "anyone_" question is false, we do not want
    to update any members because they all have an attribute as FALSE (or
    nil). In the case of citizenship, the logic is reversed: when everyone is a
    citizen we want to set an attribute ("citizen") to be TRUE.
    * This bug became apparent when filling out the 1426 PDF for a
    multi-member household where I'd said everyone was a citizen but members
    were all being marked as not a citizen because they had a `nil` value in
    the database for the `citizen` field

--- app/controllers/medicaid/intro_citizen_controller.rb
+++ app/controllers/medicaid/intro_citizen_controller.rb
@@ -7,6 +7,10 @@ module Medicaid
 
       if single_member_household?
         current_application.primary_member.update!(member_attrs)
+      elsif step_params[:everyone_a_citizen] == "true"
+        current_application.members.each do |member|
+          member.update!(citizen: true)
+        end
       end
     end
 
