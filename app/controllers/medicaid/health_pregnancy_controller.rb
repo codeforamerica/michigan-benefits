3e41b4715b2e6c9600d9d8a9ca4dd6fc42e6f28c
Jessie A. Young <jessie.a.young@gmail.com>
2017-11-16 17:46:17 -0800

    Bug: pregnancy in multi-member household w/single female
    * Usually, we update a single member if the answer to yes/no question is
    "yes".
    * Pregnancy is an edge case where if there is a multi-member household
    but only one member is a female, we can update the `new_mom` attribute
    of that single woman after the yes/no question about pregnancy.
    * This will fix a bug found via the 1426 form when "new mom" field was
    not checked for a dual household with one female. The female was not
    marked as a new mom in the database even though the `anyone_new_mom` was
    set to true on the Medicaid app.

--- app/controllers/medicaid/health_pregnancy_controller.rb
+++ app/controllers/medicaid/health_pregnancy_controller.rb
@@ -7,7 +7,15 @@ module Medicaid
 
       if single_member_household?
         current_application.primary_member.update!(member_attrs)
+      elsif female_members.count == 1
+        female_members.each do |member|
+          member.update!(member_attrs)
+        end
       end
+    end
+
+    def female_members
+      current_application.members.select(&:female?)
     end
 
     def skip?
@@ -15,9 +23,7 @@ module Medicaid
     end
 
     def all_males?
-      current_application.members.all? do |member|
-        member.sex == "male"
-      end
+      current_application.members.all?(&:male?)
     end
 
     def member_attrs
