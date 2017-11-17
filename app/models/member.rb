5b07a62d689ed0cab70fcc6e2803750c8dd0c8b2
Jessie A. Young <jessie.a.young@gmail.com>
2017-11-16 18:07:54 -0800

    Always show household members in same order on checkbox pages
    * Previously, we were using Rails default ordering, which will put the
    most recently updated record at the end.
    * This resulting in a different order of household members depending on
    who had been "updated" recently.
    * Example: I am applying as "First Person" and "Second Person" is in my
    household.
    * If I am married and Second Person is not, I will show up second in the
    list of checkboxes on questions after the marriage question, which is
    unexpected.
    * There may be better ways to do this than what I have in this PR --
    suggestions?

--- app/models/member.rb
+++ app/models/member.rb
@@ -82,6 +82,10 @@ class Member < ApplicationRecord
     end
   end
 
+  def self.ordered
+    order(created_at: :asc)
+  end
+
   def self.insured
     where(insured: true).
       where(requesting_health_insurance: true).
