8bac807a248cd2cfbc3b33f0ed32989a5284dd68
Jessie A. Young <jessie.a.young@gmail.com>
2017-11-22 12:07:57 -0800

    Remove negative assertion spec

--- spec/steps/medicaid/contact_phone_spec.rb
+++ spec/steps/medicaid/contact_phone_spec.rb
@@ -2,12 +2,6 @@ require "rails_helper"
 
 RSpec.describe Medicaid::ContactPhone do
   describe "Validations" do
-    it "does not validates presence" do
-      step = Medicaid::ContactPhone.new(phone_number: nil)
-
-      expect(step).to be_valid
-    end
-
     it "validates length" do
       step = Medicaid::ContactPhone.new(phone_number: "123123")
 
