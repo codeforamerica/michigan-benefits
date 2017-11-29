e63e738139e280ebc4d3d53d939525ab6265a0e7
Christa Hartsock <christa@codeforamerica.org>
2017-11-29 14:41:39 -0800

    Rename SNAP-only parameters to snap_application, to reduce confusion
    [#152564773]

--- app/jobs/client_email_application_job.rb
+++ app/jobs/client_email_application_job.rb
@@ -1,12 +1,12 @@
 class ClientEmailApplicationJob < ApplicationJob
   def perform(export:)
-    export.execute do |benefit_application, logger|
+    export.execute do |snap_application, logger|
       ApplicationMailer.snap_application_notification(
-        file_name: benefit_application.pdf.path,
-        recipient_email: benefit_application.email,
+        file_name: snap_application.pdf.path,
+        recipient_email: snap_application.email,
       ).deliver
 
-      logger.info("Emailed to #{benefit_application.email}")
+      logger.info("Emailed to #{snap_application.email}")
     end
   end
 end
