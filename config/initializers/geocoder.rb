9926b4df72cc735ae2fffe39358364c17a3fd1bd
Christa Hartsock <christa@codeforamerica.org>
2017-11-28 17:21:32 -0800

    Pull OpenCageData API from environment
    Fixes failing specs
    
    [#152981944]

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -7,7 +7,7 @@ Geocoder.configure(
   use_https: true,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  api_key: ENV["OPEN_CAGE_DATA_API_KEY"],               # API key for geocoding service
+  api_key: Rails.application.secrets.opencagedata_api_key,               # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
