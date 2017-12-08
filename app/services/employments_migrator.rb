f0b2cf596d4696326a4e4c5d2668394c467dce47
Paras Sanghavi <paras@codeforamerica.org>
2017-12-07 16:10:38 -0800

    Add one-off for data migration for employments
     * Made sure it was idempotent
     * Made sure it was wrapped in a transaction
    
    [#153069646]
    
    Signed-off-by: Ben Golder <bgolder@codeforamerica.org>

---
+++ app/services/employments_migrator.rb
@@ -0,0 +1,27 @@
+class EmploymentsMigrator
+  def run
+    ActiveRecord::Base.transaction do
+      members_with_jobs.each do |member|
+        last_employment_index = member.employed_number_of_jobs - 1
+        employment_indices = (0..last_employment_index).to_a
+        employments = employment_indices.map do |i|
+          Employment.new(
+            employer_name: member.employed_employer_names[i],
+            payment_frequency: member.employed_payment_frequency[i],
+            pay_quantity: member.employed_pay_quantities[i],
+          )
+        end
+
+        member.update!(employments: employments)
+      end
+    end
+  end
+
+  private
+
+  def members_with_jobs
+    Member.
+      where.not(employed_number_of_jobs: nil).
+      where("employed_number_of_jobs > ?", 0)
+  end
+end
