72bc64f80dc6645195584a6433e09e33ddfa9263
Ben Golder <bengolder@Bens-MacBook-Pro.local>
2017-12-11 15:50:27 -0800

    Link to SnapApplication from DriverError in admin
    [#153521242]

--- spec/features/admin_dashboard_driver_errors_spec.rb
+++ spec/features/admin_dashboard_driver_errors_spec.rb
@@ -18,9 +18,19 @@ RSpec.feature "Admin viewing driver errors dashboard", type: :feature do
     visit admin_root_path
 
     click_on "Driver Errors"
+
+    expect(page).to have_link(
+      driver_error.driver_application.snap_application_id,
+    )
+
     click_on "FakeError"
+
     expect(page).to have_content(
       "the error happened on a line right here, see?!",
+    )
+
+    expect(page).to have_link(
+      driver_error.driver_application.snap_application_id,
     )
   end
 end
