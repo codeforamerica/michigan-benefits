bafa6a321b9da4616680671a315a9c3c2d663399
Christa Hartsock <christa@codeforamerica.org>
2017-12-13 11:42:00 -0800

    Update uploadable styles
    * When uploading, only show "Uploading..." (not file name or delete
    link)
    * When uploaded, show green success checkmark on left of file name and
    append delete link.
    * [Finishes #152982006]
    
    Signed-off-by: Jessie Young <jessie@cylinder.work>

--- app/assets/javascripts/upload_setup.js
+++ app/assets/javascripts/upload_setup.js
@@ -16,11 +16,13 @@ var uploadSetup = function (previewTemplate) {
       var buttonHtml = button.html()
       var titleH4 = $(file.previewElement).find('h4')
       var titleText = titleH4.html()
+      var deleteLink = '<p class="text--help"><a class="link--subtle link--delete" href="#">Delete</a></p>'
 
       if (buttonHtml.indexOf('Upload more') < 0) {
         button.html(buttonHtml.replace('Upload ', 'Upload more '))
       }
-      titleH4.html(titleText.replace('Uploading ', 'Uploaded '))
+      titleH4.html(titleText.replace('Uploading...', '<span class="uploadable-status successful">✔</span> ' + file.name))
+      titleH4.append(deleteLink)
       $(file.previewElement).find('input[type="hidden"]').val(file.s3url)
     },
     addedfile: function () {
