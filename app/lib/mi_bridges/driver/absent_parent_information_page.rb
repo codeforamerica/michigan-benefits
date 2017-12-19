f0b0dbfe7b98d496d9728aa97b37b01391d99107
Jessie A. Young <jessie.a.young@gmail.com>
2017-12-19 12:00:30 -0800

    Check "no one" for common absent parent question
    * If both parents are not in the household, MIBridges asks which
    children have a common absent parent
    * We are selecting "No one" in all cases. This info can be corrected
    during an interview.
    * Also had to override `skip_infinite_loop_check` since this page shows
    up TWICE for each child, which would trigger an infinite loop check if
    there are more than 2 children in a household with an absent parent.
    * This was confirmed by driving the SNAP app with id mentioned in the
    tracker ticket.
    * [Finishes #153388256]
    https://www.pivotaltracker.com/story/show/153388256

--- app/lib/mi_bridges/driver/absent_parent_information_page.rb
+++ app/lib/mi_bridges/driver/absent_parent_information_page.rb
@@ -1,8 +1,22 @@
 module MiBridges
   class Driver
-    class AbsentParentInformationPage < ClickNextPage
+    class AbsentParentInformationPage < FillInAndClickNextPage
       def self.title
         "Absent Parent Information"
+      end
+
+      def skip_infinite_loop_check; end
+
+      def fill_in_required_fields
+        if page.has_css?("#starCommonAbsentParent")
+          check_no_one_for_common_absent_parent
+        end
+      end
+
+      private
+
+      def check_no_one_for_common_absent_parent
+        click_id("NoOne_ABSENT")
       end
     end
   end
