83c26c1fadfbd2f653db4fd3b8b757c8350cae32
Jessie A. Young <jessieay@users.noreply.github.com>
2017-12-20 14:58:05 -0800

    Fixes "MiBridges: Closing the driver causes an error" (#617)
    * The final step was calling `close`, so when the teardown method in
    `Driver.rb` tried to close the browser, it got an error because the
    driver had already been closed
    * This also fixed another bug that popped up recently where the Privacy
    PIN  page option was not being selected properly. The error showed up as
    `Selenium::WebDriver $ is not defined`. Fixed by calling `find` on the
    element so that it is loaded (Capybara waits for it) before clicking on
    the id.
    * [Finishes #153571372]

--- app/lib/mi_bridges/driver/privacy_pin_page.rb
+++ app/lib/mi_bridges/driver/privacy_pin_page.rb
@@ -6,10 +6,19 @@ module MiBridges
       end
 
       def fill_in_required_fields
-        click_id(this_is_a_private_computer_id)
+        wait_until_options_are_loaded
+        click_this_is_a_private_computer
       end
 
       private
+
+      def wait_until_options_are_loaded
+        page.find(this_is_a_private_computer_id, visible: false)
+      end
+
+      def click_this_is_a_private_computer
+        click_id(this_is_a_private_computer_id)
+      end
 
       def this_is_a_private_computer_id
         "#radioGroup_No"
