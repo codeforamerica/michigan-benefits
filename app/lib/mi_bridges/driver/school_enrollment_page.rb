f41b00219b84d62dda8bdd751a0b3ce540a3b120
Luigi Ray-Montanez <luigi@codeforamerica.org>
2017-12-20 19:03:26 -0500

    MIBridges: Fill in school data for each member.
    * On SchoolEnrollmentPage, match each member from
      SNAP application.
    * Fix failure in Driving Spec from earlier PR.
    
    [#153565851]
    
    https://www.pivotaltracker.com/story/show/153565851
    
    Signed-off-by: Jessie Young <jessie@cylinder.work>

--- app/lib/mi_bridges/driver/school_enrollment_page.rb
+++ app/lib/mi_bridges/driver/school_enrollment_page.rb
@@ -5,15 +5,21 @@ module MiBridges
         "School Enrollment"
       end
 
+      def setup
+        @current_member = find_current_member
+      end
+
       def fill_in_required_fields
-        choose_enrollment_time_for(snap_application.primary_member)
-        choose_highest_grade_completion_for(snap_application.primary_member)
+        choose_enrollment_time_for
+        choose_highest_grade_completion_for
       end
 
       private
 
-      def choose_enrollment_time_for(member)
-        selector = if member.in_college
+      attr_reader :current_member
+
+      def choose_enrollment_time_for
+        selector = if current_member.in_college
                      selector_for_radio("Full time")
                    else
                      selector_for_radio("Not in school")
@@ -22,8 +28,19 @@ module MiBridges
         js_click_selector(selector)
       end
 
-      def choose_highest_grade_completion_for(_member)
+      def choose_highest_grade_completion_for
         select("Unknown", from: "highestGradeInSchool")
+      end
+
+      def find_current_member
+        within(first("#helpContent")) do
+          snap_application.members.detect do |member|
+            member_first_name = Regexp.new(
+              Regexp.escape(member.mi_bridges_formatted_name),
+            )
+            page.has_content?(member_first_name)
+          end
+        end
       end
     end
   end
