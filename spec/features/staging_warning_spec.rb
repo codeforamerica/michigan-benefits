2e03a93bd8287df2d6ba1b52814e539fca54f23b
Luigi Ray-Montanez <luigi@codeforamerica.org>
2017-12-22 18:49:17 -0500

    Adds warning message to all pages on staging
    * Inserts <div> in main application layout when
      Rails.env.staging? is true
    
    [Resolves #153885053]
    
    Signed-off-by: Ben Golder <bgolder@codeforamerica.org>

---
+++ spec/features/staging_warning_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.feature "Warn users on staging only" do
+  scenario "Renders the staging warning message", :js do
+    # Stub out Rails.env to "staging"
+    allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new("staging"))
+
+    visit root_path
+    expect(page).to have_content("This is an example website")
+
+    within(".slab--hero") { proceed_with "Apply now" }
+    expect(page).to have_content("This is an example website")
+
+    visit '/clio'
+    expect(page).to have_content("This is an example website")
+  end
+
+  scenario "Does not render warning message when not on staging", :js do
+    visit root_path
+    expect(page).to_not have_content("This is an example website")
+  end
+end
