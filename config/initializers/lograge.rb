b351a2c822d64c828bf8bd34906fae7263690384
Luigi Ray-Montanez <luigi@codeforamerica.org>
2018-01-09 19:28:10 -0500

    Rubocop fixes

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,12 +1,11 @@
 Rails.application.configure do
   config.lograge.enabled = true
 
-
   config.lograge.custom_payload do |controller|
     {
       application_id: controller.current_application.try(:id),
       application_type: controller.current_application.try(:class),
-      current_admin_user_email: controller.current_admin_user.try(:email)
+      current_admin_user_email: controller.current_admin_user.try(:email),
     }
   end
 end
