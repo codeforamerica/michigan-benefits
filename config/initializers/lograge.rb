fe976750a71f191f85485b4ed7406dffc3a5b0bd
Luigi Ray-Montanez <luigi@codeforamerica.org>
2018-01-09 19:33:57 -0500

    Small name change

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
     {
       application_id: controller.current_application.try(:id),
       application_type: controller.current_application.try(:class),
-      current_admin_user_email: controller.current_admin_user.try(:email),
+      admin_user_email: controller.current_admin_user.try(:email),
     }
   end
 end
