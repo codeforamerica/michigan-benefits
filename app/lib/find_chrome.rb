21f6166232f4212b73b3689a31bcaa47e1e89731
Paras Sanghavi <paras@codeforamerica.org>
2018-01-11 14:39:45 -0800

    Find chrome executable on demand
     * By doing this, we will not see the finding chrome text on every test
     run

--- app/lib/find_chrome.rb
+++ app/lib/find_chrome.rb
@@ -1,33 +1,37 @@
 require "mkmf"
 
 class FindChrome
-  CHROME_PATHS = [
-    "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-    "/opt/google/chrome/chrome",
-    ENV["GOOGLE_CHROME_BIN"],
-    ENV["CHROME_PATH"],
-    find_executable("chrome"),
-  ].compact.freeze
-
-  CHROMEDRIVER_PATHS = [
-    "/usr/local/bin/chromedriver",
-    "/usr/lib/chromium-browser/chromedriver",
-    "/app/.chromedriver/bin/chromedriver",
-    ENV["CHROMEDRIVER_PATH"],
-    find_executable("chromedriver"),
-  ].compact.freeze
-
   def self.binary
-    search(CHROME_PATHS, for_binary_named: "Chrome")
+    search(chrome_paths, for_binary_named: "Chrome")
   end
 
   def self.driver
-    search(CHROMEDRIVER_PATHS, for_binary_named: "Chromedriver")
+    search(chromedriver_paths, for_binary_named: "Chromedriver")
   end
 
   def self.search(paths, for_binary_named:)
     found = paths.select { |path| File.executable? path }
     return found.first if found.any?
     raise "#{for_binary_named} not found on system"
+  end
+
+  def self.chrome_paths
+    @_chrome_paths ||= [
+      "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+      "/opt/google/chrome/chrome",
+      ENV["GOOGLE_CHROME_BIN"],
+      ENV["CHROME_PATH"],
+      find_executable("chrome"),
+    ].compact
+  end
+
+  def self.chromedriver_paths
+    @_chromedriver_paths ||= [
+      "/usr/local/bin/chromedriver",
+      "/usr/lib/chromium-browser/chromedriver",
+      "/app/.chromedriver/bin/chromedriver",
+      ENV["CHROMEDRIVER_PATH"],
+      find_executable("chromedriver"),
+    ].compact
   end
 end
