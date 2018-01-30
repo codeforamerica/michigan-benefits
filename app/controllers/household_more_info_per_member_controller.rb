c0dc4bc8fd42f1aa109d16ac9ee3f19e6bff731f
Christa Hartsock <christa@codeforamerica.org>
2018-01-30 14:16:22 -0800

    Update anyone_a and everyone_a if none or all members are selected
    [#154776076]

--- app/controllers/household_more_info_per_member_controller.rb
+++ app/controllers/household_more_info_per_member_controller.rb
@@ -1,5 +1,39 @@
 class HouseholdMoreInfoPerMemberController < ManyMemberStepsController
+  def update
+    assign_household_member_attributes
+
+    if step.valid?
+      ActiveRecord::Base.transaction { step.members.each(&:save!) }
+      update_application
+      redirect_to next_path
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def update_application
+    current_application.update!(application_attrs)
+  end
+
+  def application_attrs
+    {}.tap do |attributes|
+      attributes[:everyone_a_citizen] = true if all_members(:citizen?)
+      attributes[:anyone_disabled] = false if no_members(:disabled?)
+      attributes[:anyone_new_mom] = false if no_members(:new_mom?)
+      attributes[:anyone_in_college] = false if no_members(:in_college?)
+      attributes[:anyone_living_elsewhere] = false if no_members(:living_elsewhere?)
+    end
+  end
+
+  def all_members(attribute)
+    current_application.members.all? { |m| m.public_send(attribute) }
+  end
+
+  def no_members(attribute)
+    current_application.members.all? { |m| !m.public_send(attribute) }
+  end
 
   def member_attrs
     %i[citizen disabled new_mom in_college living_elsewhere]
