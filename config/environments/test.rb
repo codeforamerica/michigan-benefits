627e5a6c7bd00c6c0dcdcaadc4d3264f7795de2e
Luigi Ray-Montanez <luigi@codeforamerica.org>
2018-02-21 12:29:32 -0500

    Turn off eager_load in the test environment.
    Was causing a first post: NilClass error.
    
    https://stackoverflow.com/questions/44465118/rails-5-1-unknown-firstpos-nilclass-issue-reloading-application
    
    https://github.com/rails/spring/issues/519#issuecomment-348324369

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,7 +1,6 @@
 Rails.application.configure do
   config.cache_classes = true
-  config.eager_load = true
-  config.enable_dependency_loading = true
+  config.eager_load = false
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
     "Cache-Control" => "public, max-age=3600",
