3bf1e87d3c42e7d519d9ab9b215a04a31d180d03
Ben Golder <bgolder@codeforamerica.org>
2018-02-22 14:58:12 -0800

    Share member steps controller logic in a mixin

--- app/controllers/medicaid/member_steps_controller.rb
+++ app/controllers/medicaid/member_steps_controller.rb
@@ -2,49 +2,8 @@
 
 module Medicaid
   class MemberStepsController < MedicaidStepsController
+    include PerMemberControllerMixin
+
     helper_method :current_member
-
-    def current_member
-      @_current_member ||= member_from_form || member_from_querystring
-    end
-
-    def next_path
-      next_member_path || super
-    end
-
-    private
-
-    def next_member_path
-      return if next_member.nil?
-
-      decoded_step_path(params: { member: next_member.id })
-    end
-
-    def existing_attributes
-      HashWithIndifferentAccess.new(current_member.attributes)
-    end
-
-    def member_from_form
-      @_member_from_form ||= current_application.
-        members.
-        where(id: step_params[:id]).
-        first
-    end
-
-    def member_from_querystring
-      return if params[:member].blank?
-
-      current_application.members.find(params[:member])
-    end
-
-    def next_member
-      return if current_member.nil?
-
-      current_application.
-        members.
-        after(current_member).
-        limit(1).
-        first
-    end
   end
 end
---
+++ app/controllers/per_member_controller_mixin.rb
@@ -0,0 +1,44 @@
+module PerMemberControllerMixin
+  def current_member
+    @_current_member ||= member_from_form || member_from_querystring
+  end
+
+  def next_path
+    next_member_path || super
+  end
+
+  private
+
+  def next_member_path
+    return if next_member.nil?
+
+    decoded_step_path(params: { member: next_member.id })
+  end
+
+  def existing_attributes
+    HashWithIndifferentAccess.new(current_member.attributes)
+  end
+
+  def member_from_form
+    @_member_from_form ||= current_application.
+      members.
+      where(id: step_params[:id]).
+      first
+  end
+
+  def member_from_querystring
+    return if params[:member].blank?
+
+    current_application.members.find(params[:member])
+  end
+
+  def next_member
+    return if current_member.nil?
+
+    current_application.
+      members.
+      after(current_member).
+      limit(1).
+      first
+  end
+end
