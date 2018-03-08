070c5417255c2d1c7f5c2de2ca9db51ebb8b7b6b
Ben Golder <bgolder@codeforamerica.org>
2018-03-07 17:46:47 -0800

    Use a new base layout for modular HTML
    [Finishes #155042948]
    
    Signed-off-by: Luigi Ray-Montañez <luigi@codeforamerica.org>

--- app/helpers/mb_form_builder.rb
+++ app/helpers/mb_form_builder.rb
@@ -326,6 +326,23 @@ class MbFormBuilder < ActionView::Helpers::FormBuilder
     HTML
   end
 
+  def mb_yes_no_buttons(method)
+    <<~HTML.html_safe
+    <div class="form-card__buttons">
+      <div>
+        <button class="button button--nav button--full-mobile" type="submit" value="false" name="#{object_name}[#{method}]">
+          No
+        </button>
+      </div>
+      <div>
+        <button class="button button--nav button--full-mobile button--cta" type="submit" value="true" name="#{object_name}[#{method}]">
+          Yes
+        </button>
+      </div>
+    </div>
+    HTML
+  end
+
   private
 
   def mb_radio_button(method, collection, layout, help_text)
