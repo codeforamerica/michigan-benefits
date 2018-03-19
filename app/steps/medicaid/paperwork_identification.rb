9d7e9648cc5805443df64b0351615f90235c7792
Christa Hartsock <christa@codeforamerica.org>
2018-03-19 16:11:07 -0700

    Make presence validation error messages clearer
    In doing the previous commit, I noticed that our error messages were a little hard to parse since the field name wasn't included in the message, e.g. not selecting a radio button just showed the error message "can't be blank". Changed this to be a bit more verbose to bring them in line with other error messages, and to suggest a remedy for a failed presence validation that may not be because of user error.
    
    [#156055105]

--- app/steps/medicaid/paperwork_identification.rb
+++ app/steps/medicaid/paperwork_identification.rb
@@ -2,6 +2,7 @@ module Medicaid
   class PaperworkIdentification < Step
     step_attributes(:has_picture_id_for_everyone)
 
-    validates_presence_of :has_picture_id_for_everyone
+    validates :has_picture_id_for_everyone,
+      presence: { message: "Make sure to answer this question" }
   end
 end
