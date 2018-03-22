3a15e537dec6283f9f00a8edfda293af23ca95ff
Christa Hartsock <christa@codeforamerica.org>
2018-03-22 15:16:24 -0700

    Update model generator template to include navigator params

--- lib/generators/section/templates/form_model.template.rb
+++ lib/generators/section/templates/form_model.template.rb
@@ -24,4 +24,18 @@ class <%= model.camelcase %>Form < Form
   # Delete the method if you aren't updating a single HouseholdMember.
   <%- end -%>
   set_member_attributes()
+
+  <%- if options.doc? -%>
+  # Whitelist top-level parameter names for ApplicationNavigator, e.g.
+  #
+  #   given params: { form: { anyone_requesting_food: "yes" } }
+  #
+  #   set_navigator_attributes(:anyone_requesting_food)
+  #
+  # These attributes should be used for application flow, and not to fill out
+  # the application PDF.
+  #
+  # Delete the method if you aren't updating the ApplicationNavigator.
+  <%- end -%>
+  set_navigator_attributes()
 end
