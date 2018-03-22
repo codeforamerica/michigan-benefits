e59e14b5732e40b934ea3f65c1efd79786da376b
Christa Hartsock <christa@codeforamerica.org>
2018-03-22 15:38:07 -0700

    Add flash message and clear app on user offboard
    [#156142163]

--- app/controllers/integrated/decline_healthcare_controller.rb
+++ app/controllers/integrated/decline_healthcare_controller.rb
@@ -4,6 +4,12 @@ module Integrated
       application.healthcare_applying_members.count.positive?
     end
 
+    def update
+      flash[:notice] = "Please select 'Apply for FAP'"
+      session[:current_application_id] = nil
+      redirect_to(root_path)
+    end
+
     def form_class
       NullStep
     end
