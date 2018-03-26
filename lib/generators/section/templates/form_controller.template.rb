be27157479641edabcd4509b18a3c7ca16d6b24a
Christa Hartsock <christa@codeforamerica.org>
2018-03-26 10:29:19 -0700

    Update templates to be more easily run with no-doc option

--- lib/generators/section/templates/form_controller.template.rb
+++ lib/generators/section/templates/form_controller.template.rb
@@ -2,25 +2,27 @@ module Integrated
   class <%= model.camelcase %>Controller < FormsController
     <%- if options.doc? -%>
     # If controller should be skipped for any reason, uncomment the following and modify logic:
-    #
-    #   def self.skip?(application)
-    #     application.stable_housing?
-    #   end
-    #
+    <%- end -%>
+    def self.skip?(application)
+      # application.stable_housing?
+    end
+
+    <%- if options.doc? -%>
     # If updating models, uncomment the following and modify required logic:
-    #
-    #   def update_models
-    #     current_application.primary_member.update(member_params)
-    #     current_application.update(application_params)
-    #   end
-    #
+    <%- end -%>
+
+    def update_models
+      # current_application.primary_member.update(member_params)
+      # current_application.update(application_params)
+    end
+
+    <%- if options.doc? -%>
     # If no database update is needed, uncomment the following `form_class` method
     # to override the form class (otherwise form class will
     # default to <%= model.underscore %>Form):
-    #
+    <%- end -%>
     #   def form_class
     #     NullStep
     #   end
-    <%- end -%>
   end
 end
