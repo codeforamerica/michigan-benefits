cbbae925c6eb80f4490ffc8c32c00ef37fcd30de
Christa Hartsock <christa@codeforamerica.org>
2018-03-26 09:49:17 -0700

    Remove spouse as relationship option when spouse already in household
    [Finishes #156232870]

--- app/controllers/integrated/add_member_controller.rb
+++ app/controllers/integrated/add_member_controller.rb
@@ -1,5 +1,7 @@
 module Integrated
   class AddMemberController < FormsController
+    helper_method :valid_relationship_options
+
     def previous_path(*_args)
       overview_path
     end
@@ -10,6 +12,14 @@ module Integrated
 
     def form_class
       AddMemberForm
+    end
+
+    def valid_relationship_options
+      if current_application.members.any?(&:is_spouse?)
+        HouseholdMember::RELATIONSHIP_LABELS_AND_KEYS.reject { |arr| arr.second == "spouse" }
+      else
+        HouseholdMember::RELATIONSHIP_LABELS_AND_KEYS
+      end
     end
   end
 end
