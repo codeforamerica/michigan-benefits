e726f7a069b5e1ba35ea15777f7b8a6319426115
Christa Hartsock <christa@codeforamerica.org>
2018-03-27 17:01:27 -0700

    Refactor multiple members controller
    Move shared logic into MultipleMembersController.

---
+++ app/controllers/integrated/multiple_members_controller.rb
@@ -0,0 +1,30 @@
+module Integrated
+  class MultipleMembersController < FormsController
+    def edit
+      @form = form_class.new(members: members_to_update)
+    end
+
+    def update
+      @form = form_class.new(members: members_to_update)
+      @form.members.each do |member|
+        attrs = params.dig(:form, :members, member.to_param)
+        if attrs.present?
+          member.assign_attributes(attrs.permit(form_class.member_attributes))
+        end
+      end
+
+      if @form.valid?
+        ActiveRecord::Base.transaction { @form.members.each(&:save!) }
+        redirect_to next_path
+      else
+        render :edit
+      end
+    end
+
+    private
+
+    def members_to_update
+      current_application.members
+    end
+  end
+end
