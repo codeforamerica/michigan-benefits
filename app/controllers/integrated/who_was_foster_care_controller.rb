3eee4b85d168f430c4c1a27bb50373670a7605cb
Luigi Ray-Montanez <luigi@codeforamerica.org>
2018-04-02 17:40:58 -0400

    In options for foster care, include members who have a nil birthday
    [#156045620]
    
    Signed-off-by: Christa Harstock <christa@codeforamerica.org>
    Co-authored-by: Christa Harstock <christa@codeforamerica.org>

--- app/controllers/integrated/who_was_foster_care_controller.rb
+++ app/controllers/integrated/who_was_foster_care_controller.rb
@@ -4,5 +4,11 @@ module Integrated
       return true if application.single_member_household?
       !application.navigator.anyone_foster_care_at_18?
     end
+
+    def members_to_update
+      current_application.members.select do |member|
+        member.birthday.nil? || (member.age >= 18 && member.age <= 26)
+      end
+    end
   end
 end
