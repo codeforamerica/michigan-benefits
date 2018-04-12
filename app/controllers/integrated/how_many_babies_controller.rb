dcec13bac98653f8b4f53ec89ba2466cbe246953
Luigi Ray-Montanez <luigi@codeforamerica.org>
2018-04-12 14:18:55 -0700

    Extract MemberPerPageController from HowManyBabiesController
    Prep to use pattern elsewhere.
    
    [#156389976]
    
    Co-authored-by: Christa Hartsock <christa@codeforamerica.org>

--- app/controllers/integrated/how_many_babies_controller.rb
+++ app/controllers/integrated/how_many_babies_controller.rb
@@ -1,7 +1,5 @@
 module Integrated
-  class HowManyBabiesController < FormsController
-    helper_method :member_appropriate_translation_data
-
+  class HowManyBabiesController < MemberPerPageController
     def self.skip?(application)
       !application.navigator.anyone_pregnant?
     end
@@ -10,68 +8,16 @@ module Integrated
       current_member.update(member_params.slice(:baby_count))
     end
 
-    def current_member
-      @_current_member ||= member_from_querystring || first_pregnant_member
-    end
-
-    def next_path
-      next_member_path || super
-    end
-
-    def member_appropriate_translation_data
-      {
-        count: current_application.primary_member == current_member ? 0 : 1,
-        name: current_member.display_name,
-      }
-    end
-
     private
 
-    def next_member_path
-      return if next_member.nil?
-
-      section_path(self.class.to_param, params: { member: next_member.id })
+    def member_scope
+      current_application.
+        members.
+        pregnant
     end
 
-    def existing_attributes
+    def set_default_values
       current_member.baby_count ||= 1
-      HashWithIndifferentAccess.new(current_member.attributes)
-    end
-
-    def member_from_form
-      @_member_from_form ||= current_application.
-        members.
-        where(id: form_params[:id]).
-        first
-    end
-
-    def member_from_querystring
-      return if params[:member].blank?
-
-      current_application.members.find(params[:member])
-    end
-
-    def first_pregnant_member
-      current_application.
-        members.
-        pregnant.
-        limit(1).
-        first
-    end
-
-    def next_member
-      current_member = member_from_form ||
-        member_from_querystring ||
-        first_pregnant_member
-
-      return if current_member.nil?
-
-      current_application.
-        members.
-        pregnant.
-        after(current_member).
-        limit(1).
-        first
     end
   end
 end
---
+++ app/controllers/integrated/member_per_page_controller.rb
@@ -0,0 +1,79 @@
+module Integrated
+  class MemberPerPageController < FormsController
+    helper_method :member_appropriate_translation_data
+
+    def update_models
+      current_member.update(member_params)
+    end
+
+    def current_member
+      @_current_member ||= member_from_querystring || first_member
+    end
+
+    def next_path
+      next_member_path || super
+    end
+
+    def member_appropriate_translation_data
+      {
+        count: current_application.primary_member == current_member ? 0 : 1,
+        name: current_member.display_name(first_only: true),
+      }
+    end
+
+    private
+
+    # Override methods below
+
+    def set_default_values; end
+
+    def member_scope
+      current_application.members
+    end
+
+    # Don't override methods below
+
+    def first_member
+      member_scope.
+        limit(1).
+        first
+    end
+
+    def next_member_path
+      return if next_member.nil?
+
+      section_path(self.class.to_param, params: { member: next_member.id })
+    end
+
+    def existing_attributes
+      set_default_values
+      HashWithIndifferentAccess.new(current_member.attributes)
+    end
+
+    def member_from_form
+      @_member_from_form ||= current_application.
+        members.
+        where(id: form_params[:id]).
+        first
+    end
+
+    def member_from_querystring
+      return if params[:member].blank?
+
+      current_application.members.find(params[:member])
+    end
+
+    def next_member
+      current_member = member_from_form ||
+        member_from_querystring ||
+        first_member
+
+      return if current_member.nil?
+
+      member_scope.
+        after(current_member).
+        limit(1).
+        first
+    end
+  end
+end
