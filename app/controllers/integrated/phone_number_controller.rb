fd6320515e4e50c10a6454a392dd538600adbefd
Luigi Ray-Montanez <luigi@codeforamerica.org>
2018-06-05 15:52:23 -0400

    Set existing attributes for PhoneNumberController

--- app/controllers/integrated/phone_number_controller.rb
+++ app/controllers/integrated/phone_number_controller.rb
@@ -1,5 +1,9 @@
 module Integrated
   class PhoneNumberController < FormsController
+    def existing_attributes
+      HashWithIndifferentAccess.new(current_application.attributes)
+    end
+
     def update_models
       current_application.update(params_for(:application))
     end
