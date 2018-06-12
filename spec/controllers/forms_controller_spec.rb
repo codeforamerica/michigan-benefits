ff640966388cf40a997e49e89dae9515b7e8f1b6
Luigi Ray-Montanez <luigi@codeforamerica.org>
2018-06-12 17:09:14 -0400

    Update copy for single-app flows.
    - Footer title
    - Signature page
    - Congratulations page
    
    [#157953514]

--- spec/controllers/forms_controller_spec.rb
+++ spec/controllers/forms_controller_spec.rb
@@ -19,4 +19,44 @@ RSpec.describe FormsController do
       expect(FormsController.skip?(double("foo"))).to eq(false)
     end
   end
+
+  describe "#application_title" do
+    context "when no application present yet" do
+      it "returns a string for all programs" do
+        expect(controller.application_title).to eq("Food Assistance + Healthcare Coverage")
+      end
+    end
+
+    context "when only applying for food assistance" do
+      it "returns Food Assistance" do
+        current_app = create(:common_application,
+                             navigator: build(:application_navigator, applying_for_food: true))
+        session[:current_application_id] = current_app.id
+
+        expect(controller.application_title).to eq("Food Assistance")
+      end
+    end
+
+    context "when only applying for healthcare coverage" do
+      it "returns Healthcare Coverage" do
+        current_app = create(:common_application,
+                             navigator: build(:application_navigator, applying_for_healthcare: true))
+        session[:current_application_id] = current_app.id
+
+        expect(controller.application_title).to eq("Healthcare Coverage")
+      end
+    end
+
+    context "when only applying for all programs" do
+      it "returns Food Assistance + Healthcare Coverage" do
+        current_app = create(:common_application,
+                             navigator: build(:application_navigator,
+                                              applying_for_healthcare: true,
+                                              applying_for_food: true))
+        session[:current_application_id] = current_app.id
+
+        expect(controller.application_title).to eq("Food Assistance + Healthcare Coverage")
+      end
+    end
+  end
 end
