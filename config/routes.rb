0d4de6efaa3d63c10e50bac52671f2f395e889bb
Christa Hartsock <christa@codeforamerica.org>
2018-06-18 10:53:10 -0700

    Unblock integrated application routes in production
    [Finishes #158437201]

--- config/routes.rb
+++ config/routes.rb
@@ -73,16 +73,14 @@ Rails.application.routes.draw do
     end
   end
 
-  unless GateKeeper.production_environment?
-    resources :sections, controller: :forms, only: %i[index show] do
-      collection do
-        FormNavigation.all.each do |controller_class|
-          { get: :edit, put: :update }.each do |method, action|
-            match "/#{controller_class.to_param}",
-              action: action,
-              controller: controller_class.controller_path,
-              via: method
-          end
+  resources :sections, controller: :forms, only: %i[index show] do
+    collection do
+      FormNavigation.all.each do |controller_class|
+        { get: :edit, put: :update }.each do |method, action|
+          match "/#{controller_class.to_param}",
+            action: action,
+            controller: controller_class.controller_path,
+            via: method
         end
       end
     end
