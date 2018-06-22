2f777af97ca01f56056fae6b1bde444897a87e2b
Luigi Ray-Montanez <luigi@codeforamerica.org>
2018-06-22 19:58:39 -0400

    Remove “Other housing expenses” and related details controller.
    [#158482486]

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -6,7 +6,6 @@ class Expense < ApplicationRecord
     property_tax: "Property taxes",
     homeowners_insurance: "Homeowners or renters insurance",
     land_contract: "Land contract",
-    other_housing: "Other housing expenses",
   }.freeze
 
   MEDICAL_EXPENSES = {
--- spec/controllers/integrated/other_housing_details_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require "rails_helper"
-
-RSpec.describe Integrated::OtherHousingDetailsController do
-  it_behaves_like "single expense detail controller", :other_housing, :copays, requesting_food: "yes"
-end
