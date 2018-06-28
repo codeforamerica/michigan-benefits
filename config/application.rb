f94d8d2917463c2fa75c2bd9992b051ba5623214
Christa Hartsock <christa@codeforamerica.org>
2018-06-28 16:54:25 -0700

    Update Rails to 5.2
    [#156637659]

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,21 @@
 require_relative "boot"
 require_relative "../lib/delayed_job_web_logger"
+
 require "rails/all"
 
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module MichiganBenefits
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.0
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
     config.action_controller.action_on_unpermitted_parameters = :raise
 
     # Project configuration
--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -10,5 +10,7 @@
 #   inflect.uncountable %w( fish sheep )
 # end
 
-ActiveSupport::Inflector.inflections(:en) do |inflect|
-end
+# These inflection rules are supported but not enabled by default:
+# ActiveSupport::Inflector.inflections(:en) do |inflect|
+#   inflect.acronym 'RESTful'
+# end
