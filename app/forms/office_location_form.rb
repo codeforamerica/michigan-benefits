98422347bb6c831f375456fe28b8d228ed3d1cf7
Christa Hartsock <christa@codeforamerica.org>
2018-07-11 13:39:23 -0700

    Require client to choose a location
    [Finishes #158930637]

--- app/forms/office_location_form.rb
+++ app/forms/office_location_form.rb
@@ -1,3 +1,6 @@
 class OfficeLocationForm < Form
   set_attributes_for :application, :selected_office_location
+
+  validates_presence_of :selected_office_location,
+    message: "Make sure to select a location."
 end
