631b112c2a946be6d1f266a1584215221303de40
Christa Hartsock <christa@codeforamerica.org>
2018-08-20 13:20:28 -0700

    Use formbuilder to render errors for feedback form
    [#159703918]

--- app/forms/feedback_form.rb
+++ app/forms/feedback_form.rb
@@ -1,3 +1,5 @@
 class FeedbackForm < Form
   set_attributes_for :application, :feedback_rating, :feedback_comments
+
+  validates_presence_of :feedback_rating, message: "Please select a rating."
 end
