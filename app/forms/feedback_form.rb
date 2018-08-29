d3b1a383a6d35e4f22bcabbb70c7e323bed332cd
Christa Hartsock <christa@codeforamerica.org>
2018-08-29 14:41:47 -0700

    Require that any feedback be entered, not just rating
    If user enters a comment or a rating, feedback submission is considered valid.
    
    Also includes some minor styling changes.
    
    [Fixes #159703918]

--- app/forms/feedback_form.rb
+++ app/forms/feedback_form.rb
@@ -1,5 +1,10 @@
 class FeedbackForm < Form
   set_attributes_for :application, :feedback_rating, :feedback_comments
 
-  validates_presence_of :feedback_rating, message: "Please select a rating."
+  validate :feedback_entered
+
+  def feedback_entered
+    return true if feedback_rating.present? || feedback_comments.present?
+    errors.add(:feedback_rating, "Please select a rating or enter a comment.")
+  end
 end
--- spec/forms/feedback_form_spec.rb
+++ spec/forms/feedback_form_spec.rb
@@ -2,11 +2,25 @@ require "rails_helper"
 
 RSpec.describe FeedbackForm do
   describe "validations" do
-    it "requires feedback_rating" do
+    it "is invalid without feedback rating or comments" do
       form = FeedbackForm.new
 
       expect(form).not_to be_valid
       expect(form.errors[:feedback_rating]).to be_present
+    end
+
+    it "is valid with feedback rating" do
+      form = FeedbackForm.new(feedback_rating: "positive")
+
+      expect(form).to be_valid
+      expect(form.errors[:feedback_rating]).to_not be_present
+    end
+
+    it "is valid with feedback comments" do
+      form = FeedbackForm.new(feedback_comments: "best application ever")
+
+      expect(form).to be_valid
+      expect(form.errors[:feedback_rating]).to_not be_present
     end
   end
 end
