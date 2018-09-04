c1fbad4590eae3c0b040b130a765fb896ca6beb0
Christa Hartsock <christa@codeforamerica.org>
2018-09-04 10:19:20 -0700

    Return nil if no applications have feedback
    [Finishes #160078080]

--- app/services/feedback_ratings_calculator.rb
+++ app/services/feedback_ratings_calculator.rb
@@ -1,20 +1,19 @@
 class FeedbackRatingsCalculator
   def initialize(applications)
-    @applications = applications
+    @apps_with_feedback = applications.where.not(feedback_rating: "unfilled")
   end
 
   def percentage(category)
-    apps_of_type = applications.where(feedback_rating: category)
-    apps_with_feedback = applications.where.not(feedback_rating: "unfilled")
+    apps_of_type = apps_with_feedback.where(feedback_rating: category)
 
-    ((apps_of_type.count.to_f / apps_with_feedback.count.to_f) * 100).round unless applications.empty?
+    ((apps_of_type.count.to_f / apps_with_feedback.count.to_f) * 100).round unless apps_with_feedback.empty?
   end
 
   def net_satisfaction_score(positive_value: "positive", negative_value: "negative")
-    percentage(positive_value) - percentage(negative_value) unless applications.empty?
+    percentage(positive_value) - percentage(negative_value) unless apps_with_feedback.empty?
   end
 
   private
 
-  attr_reader :applications
+  attr_reader :apps_with_feedback
 end
